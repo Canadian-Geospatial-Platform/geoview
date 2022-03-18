22fe0870b2e8522495df858eca9f7102297d21e5
Saleh Yassin <yass0016@gmail.com>
2022-03-18 12:52:46 -0400

    fix(modal-issues): Fixed modal issues
    Closes #347

--- packages/geoview-core/src/core/containers/html-to-react.tsx
+++ packages/geoview-core/src/core/containers/html-to-react.tsx
@@ -1,18 +1,21 @@
 /* eslint-disable react/require-default-props */
 /* eslint-disable react/no-danger */
 import React, { CSSProperties } from "react";
+import { TypeJSONObject } from "../types/cgpv-types";
 
 interface HtmlToReactProps {
   htmlContent: string;
-  className?: string | undefined;
-  style?: CSSProperties | undefined;
+  className?: string;
+  style?: CSSProperties;
+  extraOptions?: TypeJSONObject;
 }
 
 export const HtmlToReact = (props: HtmlToReactProps): JSX.Element => {
-  const { htmlContent, className, style } = props;
+  const { htmlContent, className, style, extraOptions } = props;
 
   return (
     <div
+      {...extraOptions}
       className={className}
       style={style}
       dangerouslySetInnerHTML={{ __html: htmlContent }}
