b0355ad40e890f793765cd5a4ac76f2dcdaf0491
LarryWang888 <larry.wang.xyz@gmail.com>
2022-03-24 11:37:59 -0400

    fix(esrifeature): ESRI FeatureLayer Symbolization not working
    Close #314

--- packages/geoview-core/src/geo/layer/esri/esri-feature.ts
+++ packages/geoview-core/src/geo/layer/esri/esri-feature.ts
@@ -11,6 +11,7 @@ import {
 import { getXMLHttpRequest } from "../../../core/utils/utilities";
 import { TypeLayerConfig } from "../../../core/types/cgpv-types";
 import { generateId } from "../../../core/utils/utilities";
+import { blueCircleIcon } from "../../../core/types/marker-definitions";
 
 import { api } from "../../../api/api";
 
@@ -65,20 +66,13 @@ export class EsriFeature {
     let queryUrl = this.url.substr(-1) === "/" ? this.url : this.url + "/";
     queryUrl += "legend?f=pjson";
     // define a default blue icon
-    let iconSymbol = L.icon({
-      //iconUrl: 'http://services.arcgis.com/SgB3dZDkkUxpEHxu/ArcGIS/rest/services/huc17_accesses/FeatureServer/0/images/d66f4bf18356e409c6221a46633b935a',
-      iconUrl:
-        "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAILSURBVDhPpZRNSBRhGMf/M5u5KtnJ1TVBNizdox8giODBQwuxRkinzZMfhwgRRT14DzIKvHgRFEEFTe0gHRShQ2HmRxCU5Ucgy+JhBQ20ZNfN1//z7iQttmrODx6ed56Z/e07M88z+BdKKScjwBjfi6rNtV0V+/5DxSIxtSk165zTujwBw8on8MLa8AF6+r7gxug68HmHNetcigmUZwN1hTpCaVfQYhjGhHVacyKkSNbPhlfR1vwW2InE68kouA4MVgMVbjznYTvF+n//Fj59soSOrg9W4QKkcscvfYDfg24KO6WmhZTdHVrFVN3s6UdwHk4HMFcLVZwFP6WvDcoc4V9Y847g5nm3mYzSLGDhAdZNA15uGr6+lcvLhOVtYCaIW1z6ROiXt2mX0Q2d/OZeFGXSGnZZDOtUYoZ+IudPn9khtK+Ty3QY+K2XNqFHiJn51xCUCbCLJ1OnLTPVgfcyTnapdOs0L3ublNm0i+WYNNnd84HbmJPZvCz3PGxuF96JSz+9jBQ8GqjGoczm/+JOB3qrEOXysRxrBc2f+AwaxnxQMpsXRWQzNVC5GWgUh9QSPgac68DHbfQ3vcFVGaezkNvkziKU1VM2bJVPf10o9R4pvJgO4s7YBgyZAGla6TNpDXmbDwuhylyY5uWtlH2N/zJO0s8VxUVM9xmljDypkRBjmfGKom+6kgBwDEZ2r0wFzVAoAAAAAElFTkSuQmCC",
-      iconSize: [20, 20],
-      iconAnchor: [10, 10],
-    });
+    let iconSymbol = blueCircleIcon;
 
     let res = await axios.get(queryUrl);
 
     if (res.data.drawingInfo.renderer && res.data.drawingInfo.renderer.symbol) {
       let symbolInfo = res.data.drawingInfo.renderer.symbol;
-      iconSymbol = L.icon({
-        //iconUrl: 'http://services.arcgis.com/SgB3dZDkkUxpEHxu/ArcGIS/rest/services/huc17_accesses/FeatureServer/0/images/d66f4bf18356e409c6221a46633b935a',
+      iconSymbol = new L.Icon({
         iconUrl: `data:${symbolInfo.contentType};base64,${symbolInfo.imageData}`,
         iconSize: [symbolInfo.width, symbolInfo.height],
         iconAnchor: [
@@ -139,7 +133,6 @@ export class EsriFeature {
     let queryUrl = this.url.substr(-1) === "/" ? this.url : this.url + "/";
     queryUrl += "legend?f=pjson";
     return axios.get(queryUrl).then((res) => {
-      console.log(res.data);
       return res.data;
     });
   };
