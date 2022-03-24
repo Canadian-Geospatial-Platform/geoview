859712dbf1fc8e2248e0dfbf2ea201bccb21978a
LarryWang888 <larry.wang.xyz@gmail.com>
2022-03-24 11:00:14 -0400

    fix(esrifeature): ESRI FeatureLayer Symbolization not working
    Close #314

--- packages/geoview-core/src/geo/layer/esri/esri-feature.ts
+++ packages/geoview-core/src/geo/layer/esri/esri-feature.ts
@@ -61,7 +61,33 @@ export class EsriFeature {
    * @param {TypeLayerConfig} layer the layer configuration
    * @return {Promise<Layer | string>} layers to add to the map
    */
-  add(layer: TypeLayerConfig): Promise<Layer | string> {
+  async add(layer: TypeLayerConfig): Promise<Layer | string> {
+    let queryUrl = this.url.substr(-1) === "/" ? this.url : this.url + "/";
+    queryUrl += "legend?f=pjson";
+    // define a default blue icon
+    let iconSymbol = L.icon({
+      //iconUrl: 'http://services.arcgis.com/SgB3dZDkkUxpEHxu/ArcGIS/rest/services/huc17_accesses/FeatureServer/0/images/d66f4bf18356e409c6221a46633b935a',
+      iconUrl:
+        "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAILSURBVDhPpZRNSBRhGMf/M5u5KtnJ1TVBNizdox8giODBQwuxRkinzZMfhwgRRT14DzIKvHgRFEEFTe0gHRShQ2HmRxCU5Ucgy+JhBQ20ZNfN1//z7iQttmrODx6ed56Z/e07M88z+BdKKScjwBjfi6rNtV0V+/5DxSIxtSk165zTujwBw8on8MLa8AF6+r7gxug68HmHNetcigmUZwN1hTpCaVfQYhjGhHVacyKkSNbPhlfR1vwW2InE68kouA4MVgMVbjznYTvF+n//Fj59soSOrg9W4QKkcscvfYDfg24KO6WmhZTdHVrFVN3s6UdwHk4HMFcLVZwFP6WvDcoc4V9Y847g5nm3mYzSLGDhAdZNA15uGr6+lcvLhOVtYCaIW1z6ROiXt2mX0Q2d/OZeFGXSGnZZDOtUYoZ+IudPn9khtK+Ty3QY+K2XNqFHiJn51xCUCbCLJ1OnLTPVgfcyTnapdOs0L3ublNm0i+WYNNnd84HbmJPZvCz3PGxuF96JSz+9jBQ8GqjGoczm/+JOB3qrEOXysRxrBc2f+AwaxnxQMpsXRWQzNVC5GWgUh9QSPgac68DHbfQ3vcFVGaezkNvkziKU1VM2bJVPf10o9R4pvJgO4s7YBgyZAGla6TNpDXmbDwuhylyY5uWtlH2N/zJO0s8VxUVM9xmljDypkRBjmfGKom+6kgBwDEZ2r0wFzVAoAAAAAElFTkSuQmCC",
+      iconSize: [20, 20],
+      iconAnchor: [10, 10],
+    });
+
+    let res = await axios.get(queryUrl);
+
+    if (res.data.drawingInfo.renderer && res.data.drawingInfo.renderer.symbol) {
+      let symbolInfo = res.data.drawingInfo.renderer.symbol;
+      iconSymbol = L.icon({
+        //iconUrl: 'http://services.arcgis.com/SgB3dZDkkUxpEHxu/ArcGIS/rest/services/huc17_accesses/FeatureServer/0/images/d66f4bf18356e409c6221a46633b935a',
+        iconUrl: `data:${symbolInfo.contentType};base64,${symbolInfo.imageData}`,
+        iconSize: [symbolInfo.width, symbolInfo.height],
+        iconAnchor: [
+          Math.round(symbolInfo.width / 2),
+          Math.round(symbolInfo.height / 2),
+        ],
+      });
+    }
+
     const data = getXMLHttpRequest(`${layer.url}?f=json`);
 
     const geo = new Promise<Layer | string>((resolve) => {
@@ -77,6 +103,9 @@ export class EsriFeature {
         ) {
           const feat = featureLayer({
             url: layer.url,
+            pointToLayer: function (feature, latlng) {
+              return L.marker(latlng, { icon: iconSymbol });
+            },
           });
 
           resolve(feat);
@@ -110,6 +139,7 @@ export class EsriFeature {
     let queryUrl = this.url.substr(-1) === "/" ? this.url : this.url + "/";
     queryUrl += "legend?f=pjson";
     return axios.get(queryUrl).then((res) => {
+      console.log(res.data);
       return res.data;
     });
   };
