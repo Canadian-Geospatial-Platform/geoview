7fc282896b5e4bc5de5fa7e397a5210f9e450757
jolevesq <johann.levesque@canada.ca>
2022-11-30 16:11:02 -0500

    feat(data-grid): Add a custom key field and set table style Closes #619, #616

--- packages/geoview-core/src/core/components/data-grid/layer-data-grid.tsx
+++ packages/geoview-core/src/core/components/data-grid/layer-data-grid.tsx
@@ -1,4 +1,7 @@
-import { DataGrid, DataGridProps } from '@mui/x-data-grid';
+import { DataGrid, DataGridProps, gridClasses, GridToolbar, GridCellParams, frFR, enUS } from '@mui/x-data-grid';
+
+import { TypeDisplayLanguage } from '../../../geo/map/map-schema-types';
+import { Tooltip } from '../../../ui';
 
 /**
  * Create a data grid (table) component for a lyer features all request
@@ -6,14 +9,96 @@ import { DataGrid, DataGridProps } from '@mui/x-data-grid';
  * @param {DataGridProps} props table properties
  * @returns {JSX.Element} returns table component
  */
-export function LayerDataGrid(props: DataGridProps) {
+
+// extend the DataGridProps to include the key row element
+interface CustomDataGridProps extends DataGridProps {
+  rowId: string;
+  displayLanguage: TypeDisplayLanguage;
+}
+
+const sxClasses = {
+  DataGrid: {
+    boxShadow: 2,
+    border: 2,
+    borderColor: 'primary.light',
+    '& .MuiDataGrid-cell:hover': {
+      color: 'text.primary',
+    },
+    [`& div.even.${gridClasses.row}`]: {
+      backgroundColor: 'grey.200',
+      '&:hover, &.Mui-hovered': {
+        backgroundColor: 'action.hoverRow',
+        '@media (hover: none)': {
+          backgroundColor: 'transparent',
+        },
+      },
+      '&.Mui-selected': {
+        backgroundColor: 'action.selectedRow',
+        '&:hover, &.Mui-hovered': {
+          backgroundColor: 'action.hoverRow',
+          // Reset on touch devices, it doesn't add specificity
+          '@media (hover: none)': {
+            backgroundColor: 'action.selectedRow',
+          },
+        },
+      },
+    },
+    [`& .${gridClasses.row}`]: {
+      '&:hover, &.Mui-hovered': {
+        backgroundColor: 'action.hoverRow',
+        '@media (hover: none)': {
+          backgroundColor: 'transparent',
+        },
+      },
+      '&.Mui-selected': {
+        backgroundColor: 'action.selectedRow',
+        '&:hover, &.Mui-hovered': {
+          backgroundColor: 'action.hoverRow',
+          // Reset on touch devices, it doesn't add specificity
+          '@media (hover: none)': {
+            backgroundColor: 'action.selectedRow',
+          },
+        },
+      },
+    },
+  },
+};
+
+export function LayerDataGrid(props: CustomDataGridProps) {
+  const { rowId, displayLanguage, columns } = props;
+
+  // tooltip implementation for column content
+  // TODO: works only with hover and add tooltips even when not needed. need improvement
+  columns.forEach((column) => {
+    // eslint-disable-next-line no-param-reassign
+    column.renderCell = (params: GridCellParams) => (
+      <Tooltip title={params.value}>
+        <span style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>{params.value}</span>
+      </Tooltip>
+    );
+  });
+
+  // set locale from display language
+  const locale =
+    displayLanguage === 'en' ? enUS.components.MuiDataGrid.defaultProps.localeText : frFR.components.MuiDataGrid.defaultProps.localeText;
+
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        {...props}
-        getRowId={(row) => row.OBJECTID}
-        getRowClassName={(params) => (params.indexRelativeToCurrentPage % 2 === 0 ? 'even' : 'odd')}
-      />
+    <div style={{ display: 'flex', height: '100%' }}>
+      <div style={{ flexGrow: 1 }}>
+        <DataGrid
+          localeText={locale}
+          sx={sxClasses.DataGrid}
+          {...props}
+          getRowId={(row) => row[rowId]}
+          getRowClassName={(params) => (params.indexRelativeToCurrentPage % 2 === 0 ? 'even' : 'odd')}
+          checkboxSelection
+          disableSelectionOnClick
+          rowsPerPageOptions={[50]}
+          components={{
+            Toolbar: GridToolbar,
+          }}
+        />
+      </div>
     </div>
   );
 }
