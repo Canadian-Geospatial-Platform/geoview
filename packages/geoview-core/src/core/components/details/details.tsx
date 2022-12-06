a16abf3edfbc7562ecae07e91ace5aa1abb7e37e
Dennis <dennis.fu@canada.ca>
2022-12-06 12:17:07 -0500

    final changes

--- packages/geoview-core/src/core/components/details/details.tsx
+++ packages/geoview-core/src/core/components/details/details.tsx
@@ -21,58 +21,82 @@ export interface TypeLayerSetData {
  */
 export function Details(): JSX.Element | null {
   const { mapId } = useContext(MapContext);
-  // const mapInstance = api.map(mapId).map;
+  const mapInstance = api.map(mapId).map;
   // get event names
   const EVENT_NAMES = api.eventNames;
   // const { geoviewLayers } = api.map(mapId).layer;
 
-  const { t, i18n } = useTranslation<string>();
+  const { i18n } = useTranslation<string>();
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [lnglat, setLnglat] = useState<number[]>([]);
   const [layersData, setLayersData] = useState<Record<string, TypeLayerSetData>>({});
-  
+
   useEffect(() => {
     // get the map service layers from the API
-    const mapLayers = api.map(mapId).layer.geoviewLayers;
-    // console.log(api.map(mapId).layer);
+    // let layerSetData: Record<string, TypeLayerSetData> = {};
+    mapInstance.on('click', (e: { originalEvent: { shiftKey: unknown }; coordinate: number[] }) => {
+      if (!e.originalEvent.shiftKey) {
+        setLnglat(e.coordinate);
+        setLayersData({});
+        // layerSetData = {};
+        api.event.off(EVENT_NAMES.GET_FEATURE_INFO.ALL_QUERIES_DONE, mapId);
+        const mapLayers = api.map(mapId).layer.geoviewLayers;
+        // console.log(api.map(mapId).layer);
 
-    // loop through each map server layer loaded from the map config and created using the API
-    const arrayOfgeoviewLayerId = Object.keys(mapLayers);
+        // loop through each map server layer loaded from the map config and created using the API
+        const arrayOfgeoviewLayerId = Object.keys(mapLayers);
 
-    arrayOfgeoviewLayerId.forEach(async (geoviewLayerId: string) => {
-      const mapLayer = mapLayers[geoviewLayerId];
-      // console.log(mapLayer);
-      const featureInfoLayerSet = api.createFeatureInfoLayerSet(mapId, mapLayer.geoviewLayerId);
-      const layerSetName = mapLayer.geoviewLayerName[i18n.language as TypeDisplayLanguage];
-      if (featureInfoLayerSet) {
-        // console.log(featureInfoEsriFeatureLayerSet);
-        api.event.on(
-          EVENT_NAMES.GET_FEATURE_INFO.ALL_QUERIES_DONE,
-          (payload: any) => {
-            const { layerSetId, resultSets } = payload;
-            const layerData: Record<string, TypeLayerData> = {};
-            Object.keys(resultSets).forEach((layerId: string) => {
-              const subId = layerId.replace(`${layerSetId}/`, '');
-              const subLayer = mapLayer?.listOfLayerEntryConfig?.find((l) => {
-                return l.layerId == subId;
-              });
-              if (subLayer && subLayer.layerName) {
-                const layerName = subLayer.layerName[i18n.language as TypeDisplayLanguage];
-                layerData[layerId] = { layerName: layerName === undefined ? 'UNDEFINED' : layerName, features: resultSets[layerId] };
-              }
-            });
-            const layerSetData: Record<string, TypeLayerSetData> = {};
-            layerSetData[layerSetId] = { layerSetName: layerSetName === undefined ? 'UNDEFINED' : layerSetName, layerData };
-            setLayersData(layerSetData);
-          },
-          mapId
-        );
+        arrayOfgeoviewLayerId.forEach(async (geoviewLayerId: string) => {
+          const mapLayer = mapLayers[geoviewLayerId];
+          // console.log(mapLayer);
+          const featureInfoLayerSet = api.createFeatureInfoLayerSet(mapId, mapLayer.geoviewLayerId);
+          const layerSetName = mapLayer.geoviewLayerName[i18n.language as TypeDisplayLanguage];
+          if (featureInfoLayerSet) {
+            // console.log(featureInfoEsriFeatureLayerSet);
+            api.event.on(
+              EVENT_NAMES.GET_FEATURE_INFO.ALL_QUERIES_DONE,
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              (payload: any) => {
+                const { layerSetId, resultSets } = payload;
+                const layerData: Record<string, TypeLayerData> = {};
+                Object.keys(resultSets).forEach((layerId: string) => {
+                  if (resultSets[layerId].length > 0) {
+                    const subId = layerId.replace(`${layerSetId}/`, '');
+                    const subLayer = mapLayer?.listOfLayerEntryConfig?.find((l) => {
+                      // eslint-disable-next-line eqeqeq
+                      return l.layerId == subId;
+                    });
+                    const layerName =
+                      subLayer && subLayer.layerName ? subLayer.layerName[i18n.language as TypeDisplayLanguage] : 'UNDEFINED';
+                    layerData[layerId] = { layerName: layerName === undefined ? 'UNDEFINED' : layerName, features: resultSets[layerId] };
+                  }
+                });
+
+                // layerSetData[layerSetId] = { layerSetName: layerSetName === undefined ? 'UNDEFINED' : layerSetName, layerData };
+                if (Object.keys(layerData).length > 0) {
+                  setLayersData((prevState) => ({
+                    ...prevState,
+                    [layerSetId]: {
+                      ...prevState[layerSetId],
+                      layerSetName: layerSetName === undefined ? 'UNDEFINED' : layerSetName,
+                      layerData,
+                    },
+                  }));
+                }
+              },
+              mapId
+            );
+          }
+        });
       }
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <div>{t('click_map')}</div>
+    <div>
+      {/* <span>{lnglat.length > 0 && JSON.stringify(lnglat)}</span> */}
       <LayersList layersData={layersData} />
     </div>
   );
--- packages/geoview-core/src/core/components/details/layers-list.tsx
+++ packages/geoview-core/src/core/components/details/layers-list.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/require-default-props */
 import React, { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 import Box from '@mui/material/Box';
 import {
   Collapse,
@@ -9,17 +9,11 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
-  CloseIcon,
   ExpandMoreIcon,
   ExpandLessIcon,
-  TodoIcon,
-  ListAltIcon,
   Tooltip,
-  VisibilityIcon,
-  VisibilityOffIcon,
   IconButton,
 } from '../../../ui';
-import { AbstractGeoViewLayer } from '../../../app';
 import { TypeJsonObject } from '../../types/global-types';
 import { TypeLayerSetData } from './details';
 import { FeatureInfo } from './feature-info';
@@ -27,7 +21,6 @@ import { FeatureInfo } from './feature-info';
 const sxClasses = {
   details: {
     width: '100%',
-    // maxWidth: 350, // for testing panel width
   },
   layerItem: {
     color: 'text.primary',
@@ -54,16 +47,16 @@ interface TypeLayersListProps {
 }
 
 /**
- * Legend Item for a Legend list
+ * layers list
  *
- * @returns {JSX.Element} the legend list item
+ * @returns {JSX.Element} the layers list
  */
 export function LayersList(props: TypeLayersListProps): JSX.Element {
   const { layersData } = props;
-  const { t, i18n } = useTranslation<string>();
+  // const { t, i18n } = useTranslation<string>();
   const [layerSetOpen, setLayerSetOpen] = useState<string>('');
   const [layerOpen, setLayerOpen] = useState<string>('');
-  
+
   useEffect(() => {
     const layerSetIds = Object.keys(layersData);
     if (layerSetIds.length === 1) {
@@ -75,69 +68,87 @@ export function LayersList(props: TypeLayersListProps): JSX.Element {
     }
   }, [layersData]);
 
-  // useEffect(() => {
-  //   if (layerConfigEntry) {
-  //     rootGeoViewLayer.setVisible(isChecked, layerConfigEntry);
-  //   } else {
-  //     rootGeoViewLayer.setVisible(isChecked);
-  //   }
-  // }, [isChecked, layerConfigEntry, rootGeoViewLayer]);
-
   return (
     <List sx={sxClasses.details}>
       {
         // loop through each map server layer
         Object.keys(layersData).map((layerSetId) => {
-          const {layerSetName, layerData} = layersData[layerSetId];
+          const { layerSetName, layerData } = layersData[layerSetId];
 
           return (
             <div key={layerSetId}>
-            <ListItem sx={sxClasses.layerItem} onClick={() => setLayerSetOpen(layerSetOpen !== layerSetId? layerSetId : '')}>
-              <ListItemButton>
-              <Tooltip title={layerSetId} placement="top" enterDelay={1000}>
-                <ListItemText primaryTypographyProps={{ fontSize: 14, noWrap: true }} primary={layerSetName} />
-              </Tooltip>
-              </ListItemButton>
-            </ListItem>
-            <Collapse in={layerSetOpen === layerSetId} timeout="auto" unmountOnExit>
-            <Box>
-              <Box sx={sxClasses.expandableIconContainer}>  
-              {
-                // loop through each layer in the map server
-                Object.keys(layerData).map((layerId) => {
-                  const { layerName, features } = layerData[layerId];
+              <ListItem sx={sxClasses.layerItem} onClick={() => setLayerSetOpen(layerSetOpen !== layerSetId ? layerSetId : '')}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <IconButton color="primary">{layerSetOpen !== layerSetId ? <ExpandMoreIcon /> : <ExpandLessIcon />}</IconButton>
+                    {/* {iconType === 'simple' && iconImg !== null && (
+                      <Avatar sx={sxClasses.legendIcon} variant="square" src={isLegendOpen ? '' : iconImg} onClick={() => handleLegendClick()}>
+                        <CloseIcon />
+                      </Avatar>
+                    )}
+                    {iconType === 'list' && (
+                      <Avatar sx={sxClasses.legendIcon} variant="square" onClick={() => handleLegendClick()}>
+                        {isLegendOpen ? <CloseIcon /> : <ListAltIcon />}
+                      </Avatar>
+                    )}
+                    {groupItems.length === 0 && !iconType && (
+                      <Avatar sx={sxClasses.legendIcon} variant="square" onClick={() => handleLegendClick()}>
+                        <TodoIcon />
+                      </Avatar>
+                    )} */}
+                  </ListItemIcon>
+                  <Tooltip title={layerSetId} placement="top" enterDelay={1000}>
+                    <ListItemText primaryTypographyProps={{ fontSize: 14, noWrap: true }} primary={layerSetName} />
+                  </Tooltip>
+                </ListItemButton>
+              </ListItem>
+              <Collapse in={layerSetOpen === layerSetId} timeout="auto" unmountOnExit>
+                <Box>
+                  <Box sx={sxClasses.expandableIconContainer}>
+                    {
+                      // loop through each layer in the map server
+                      Object.keys(layerData).map((layerId) => {
+                        const { layerName, features } = layerData[layerId];
 
-                  return (
-                    <div key={layerId}>
-                    <ListItem sx={sxClasses.layerItem} onClick={() => setLayerOpen(layerOpen !== layerId? layerId : '')}>
-                      <ListItemButton>
-                      <Tooltip title={layerId} placement="top" enterDelay={1000}>
-                        <ListItemText primaryTypographyProps={{ fontSize: 14, noWrap: true }} primary={layerName} />
-                      </Tooltip>
-                      </ListItemButton>
-                    </ListItem>
-                    <Collapse in={layerOpen === layerId} timeout="auto" unmountOnExit>
-                      <Box>
-                        <Box sx={sxClasses.expandableIconContainer}>
-                            {Array.isArray(features) &&
-                            features.map((feature: TypeJsonObject, index: number) => {
-                              return <FeatureInfo key={index} feature={feature} />
-                            })}
-                        </Box>
-                      </Box>
-                    </Collapse>
-                    </div>
-                  );
-                })
-              }
-              </Box>
-            </Box>
-            </Collapse>
+                        return (
+                          <div key={layerId}>
+                            <ListItem
+                              sx={sxClasses.layerItem}
+                              onClick={() => setLayerOpen(layerSetOpen !== layerSetId || layerOpen !== layerId ? layerId : '')}
+                            >
+                              <ListItemButton>
+                                <ListItemIcon>
+                                  <IconButton color="primary">
+                                    {layerSetOpen !== layerSetId || layerOpen !== layerId ? <ExpandMoreIcon /> : <ExpandLessIcon />}
+                                  </IconButton>
+                                </ListItemIcon>
+                                <Tooltip title={layerId} placement="top" enterDelay={1000}>
+                                  <ListItemText primaryTypographyProps={{ fontSize: 14, noWrap: true }} primary={layerName} />
+                                </Tooltip>
+                              </ListItemButton>
+                            </ListItem>
+                            <Collapse in={layerOpen === layerId} timeout="auto" unmountOnExit>
+                              <Box>
+                                <Box sx={sxClasses.expandableIconContainer}>
+                                  {Array.isArray(features) &&
+                                    features.map((feature: TypeJsonObject, index: number) => {
+                                      // eslint-disable-next-line react/no-array-index-key
+                                      return <FeatureInfo key={index} feature={feature} />;
+                                    })}
+                                </Box>
+                              </Box>
+                            </Collapse>
+                          </div>
+                        );
+                      })
+                    }
+                  </Box>
+                </Box>
+              </Collapse>
             </div>
           );
         })
       }
     </List>
   );
-
 }
