71e6b494adf2144eaf60b67648bd194e87f63310
Dennis <dennis.fu@canada.ca>
2023-02-15 20:39:34 -0500

    rebased latest updates

--- packages/geoview-core/src/core/components/details/details.tsx
+++ packages/geoview-core/src/core/components/details/details.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { Coordinate } from 'ol/coordinate';
 import { TypeArrayOfFeatureInfoEntries } from '../../../api/events/payloads/get-feature-info-payload';
 import { LayersList } from './layers-list';
 
 export interface DetailsProps {
   mapId: string;
-  location: unknown;
+  location: Coordinate;
   backgroundStyle?: string;
   singleColumn?: boolean;
+  handlerName: string | null;
 }
 export interface TypeDetailsProps {
   arrayOfLayerData: TypeArrayOfLayerData;
@@ -28,11 +30,12 @@ export type TypeArrayOfLayerData = TypeLayerData[];
 export function Details(props: TypeDetailsProps): JSX.Element | null {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { arrayOfLayerData, detailsSettings } = props;
+  const { location, handlerName } = detailsSettings;
   const [details, setDetails] = useState<TypeArrayOfLayerData>([]);
 
   useEffect(() => {
     setDetails(arrayOfLayerData);
-  }, [arrayOfLayerData]);
+  }, [arrayOfLayerData, location, handlerName]);
 
   return <LayersList arrayOfLayerData={details} detailsSettings={detailsSettings} />;
 }
