e0cf8626dd7caf2fd725dfb7028552fa971c0af5
Alex-NRCan <alexandre.roy@nrcan-rncan.gc.ca>
2023-03-15 13:52:06 -0400

    Cleanup of class for eslint

--- packages/geoview-core/src/ui/accordion/accordion.tsx
+++ packages/geoview-core/src/ui/accordion/accordion.tsx
@@ -1,27 +1,24 @@
-import { useRef, useState, useEffect, useCallback } from 'react';
-
 import {
-	Accordion as MaterialAccordion, 
-	AccordionSummary as MaterialAccordionSummary, 
-	AccordionDetails as MaterialAccordionDetails
-} from "@mui/material";
+  Accordion as MaterialAccordion,
+  AccordionSummary as MaterialAccordionSummary,
+  AccordionDetails as MaterialAccordionDetails,
+} from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { generateId } from "../../core/utils/utilities";
-
+import { generateId } from '../../core/utils/utilities';
 
 /**
  * Properties for the Accordion element
  */
 interface AccordionProps {
-	id?: number;
-	items: Array<AccordionItem>;
-	className: string;
+  id: string;
+  items: Array<AccordionItem>;
+  className: string;
 }
 
 export type AccordionItem = {
-	title: string;
-	content: React.ReactNode | Element;	
-}
+  title: string;
+  content: React.ReactNode | Element;
+};
 
 /**
  * Create a customized Material UI Fade
@@ -29,32 +26,23 @@ export type AccordionItem = {
  * @param {AccordionProps} props the properties passed to the Fade element
  * @returns {JSX.Element} the created Fade element
  */
-export const Accordion = (props: AccordionProps): JSX.Element => {
-  const { id, items, className} = props;
-  
-  return <div
-        id={generateId(id?.toString())}
-        className={"accordion-group"}>
-        {Object.values(items).map((item: AccordionItem, idx: number) => (
-          <MaterialAccordion
-          key={idx}
-          className={className}
-        >
-          <MaterialAccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls={"accordion-panel-" + idx + "-a-content"}
-          >
-            <div>
-              {item.title}
-            </div>
+export function Accordion(props: AccordionProps): JSX.Element {
+  const { id, items, className } = props;
+
+  return (
+    <div id={generateId(id)} className="accordion-group">
+      {Object.values(items).map((item: AccordionItem, idx: number) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <MaterialAccordion key={idx} className={className}>
+          <MaterialAccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={`accordion-panel-${idx}-a-content`}>
+            <div>{item.title}</div>
           </MaterialAccordionSummary>
-          <MaterialAccordionDetails>
-            {item.content}
-          </MaterialAccordionDetails>
-  			</MaterialAccordion>
-  		))}
-	</div>;
-};
+          <MaterialAccordionDetails>{item.content}</MaterialAccordionDetails>
+        </MaterialAccordion>
+      ))}
+    </div>
+  );
+}
 
 /**
  * Example of usage
@@ -68,4 +56,3 @@ export const Accordion = (props: AccordionProps): JSX.Element => {
       ))}
   ></Accordion>
  */
-
