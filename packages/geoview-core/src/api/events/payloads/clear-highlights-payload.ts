9adedc17b5a0bcd66893e4413142f586e2ab2a19
Damon Ulmi <damon_ulmi@hotmail.com>
2023-06-30 10:36:57 -0700

    feature(ui): Select feature from details/highlight selected detail
    Closes #1094
    Closes #1095

---
+++ packages/geoview-core/src/api/events/payloads/clear-highlights-payload.ts
@@ -0,0 +1,56 @@
+import { PayloadBaseClass } from './payload-base-class';
+
+import { EventStringId, EVENT_NAMES } from '../event-types';
+
+/** Valid events that can create ClearHighlightsPayload */
+const validEvents: EventStringId[] = [EVENT_NAMES.FEATURE_HIGHLIGHT.EVENT_HIGHLIGHT_CLEAR];
+
+/**
+ * type guard function that redefines a PayloadBaseClass as a ClearHighlightsPayload
+ * if the event attribute of the verifyIfPayload parameter is valid. The type ascention
+ * applies only to the true block of the if clause.
+ *
+ * @param {PayloadBaseClass} verifyIfPayload object to test in order to determine if the type ascention is valid
+ * @returns {boolean} returns true if the payload is valid
+ */
+export const payloadIsAClearHighlights = (verifyIfPayload: PayloadBaseClass): verifyIfPayload is ClearHighlightsPayload => {
+  return validEvents.includes(verifyIfPayload?.event);
+};
+
+/**
+ * Class definition for ClearHighlightsPayload
+ *
+ * @exports
+ * @class ClearHighlightsPayload
+ */
+export class ClearHighlightsPayload extends PayloadBaseClass {
+  // the Uid of the feature to remove highlights from, or 'all' to clear all
+  id: string;
+
+  /**
+   * Constructor for the class
+   *
+   * @param {EventStringId} event the event identifier for which the payload is constructed
+   * @param {string | null} handlerName the handler Name
+   * @param {string} id the Uid of the feature to remove highlight from or 'all'
+   */
+  constructor(event: EventStringId, handlerName: string | null, id: string) {
+    if (!validEvents.includes(event)) throw new Error(`ClearHighlightsPayload can't be instanciated for event of type ${event}`);
+    super(event, handlerName);
+    this.id = id;
+  }
+}
+
+/**
+ * Helper function used to instanciate a ClearHighlightsPayload object. This function
+ * avoids the "new ClearHighlightsPayload" syntax.
+ *
+ * @param {EventStringId} event the event identifier for which the payload is constructed
+ * @param {string | null} handlerName the handler Name
+ * @param {string} id the Uid of the feature to remove highlight from or 'all'
+ *
+ * @returns {FeatureHighlightPayload} the FeatureHighlightPayload object created
+ */
+export const clearHighlightsPayload = (event: EventStringId, handlerName: string | null, id: string): ClearHighlightsPayload => {
+  return new ClearHighlightsPayload(event, handlerName, id);
+};
--- packages/geoview-core/src/core/components/details/feature-info.tsx
+++ packages/geoview-core/src/core/components/details/feature-info.tsx
@@ -1,5 +1,8 @@
 /* eslint-disable react/require-default-props */
-import React, { useEffect, useState } from 'react';
+import React, { MutableRefObject, useEffect, useState } from 'react';
+
+import { getUid } from 'ol/util';
+
 import { useTheme, Theme } from '@mui/material/styles';
 import MaterialCardMedia from '@mui/material/CardMedia';
 import { useTranslation } from 'react-i18next';
@@ -16,17 +19,27 @@ import {
   Tooltip,
   IconButton,
   Box,
+  CheckBoxIcon,
+  CheckBoxOutIcon,
 } from '../../../ui';
 import { api } from '../../../app';
 import { TypeFeatureInfoEntry, TypeFieldEntry } from '../../../api/events/payloads/get-feature-info-payload';
 import { DetailsProps } from './details';
 import { isImage, stringify, generateId } from '../../utils/utilities';
 import { LightboxImg, LightBoxSlides } from '../lightbox/lightbox';
+import { EVENT_NAMES } from '../../../api/events/event-types';
+import { featureHighlightPayload } from '../../../api/events/payloads/feature-highlight-payload';
+import { clearHighlightsPayload } from '../../../api/events/payloads/clear-highlights-payload';
 
 const sxClasses = {
   layerItem: {
+    color: 'text.secondary',
+    padding: 0,
+  },
+  layerItemSelected: {
     color: 'text.primary',
     padding: 0,
+    border: 'solid',
   },
   itemText: {
     fontSize: 14,
@@ -74,6 +87,7 @@ export interface TypeFeatureProps {
   // eslint-disable-next-line react/no-unused-prop-types
   feature: TypeFeatureInfoEntry;
   startOpen?: boolean;
+  selectedFeatures?: MutableRefObject<string[]>;
   detailsSettings: DetailsProps;
 }
 
@@ -85,11 +99,13 @@ export interface TypeFeatureProps {
 export function FeatureInfo(props: TypeFeatureProps): JSX.Element {
   const { t } = useTranslation<string>();
 
-  const { feature, startOpen, detailsSettings } = props;
+  const { feature, startOpen, selectedFeatures, detailsSettings } = props;
   const { mapId } = detailsSettings;
   const featureIconSrc = feature.featureIcon.toDataURL();
+  const featureUid = getUid(feature.geometry);
   const nameFieldValue = feature.fieldInfo[feature.nameField!]!.value as string;
   const [isOpen, setOpen] = useState<boolean>(false);
+  const [isSelected, setIsSelected] = useState<boolean>(false);
   const featureInfoList: TypeFieldEntry[] = Object.keys(feature.fieldInfo).map((fieldName) => {
     return {
       fieldKey: feature.fieldInfo[fieldName]!.fieldKey,
@@ -101,7 +117,7 @@ export function FeatureInfo(props: TypeFeatureProps): JSX.Element {
   });
 
   // lightbox component state
-  const [isLightboxOpen, setIsLighboxOpen] = useState(false);
+  const [isLightboxOpen, setIsLightboxOpen] = useState(false);
   const [slides, setSlides] = useState<LightBoxSlides[]>([]);
   const [slidesIndex, setSlidesIndex] = useState(0);
 
@@ -109,10 +125,36 @@ export function FeatureInfo(props: TypeFeatureProps): JSX.Element {
     iconImg: React.CSSProperties;
   } = useTheme();
 
+  useEffect(() => {
+    if (selectedFeatures?.current && selectedFeatures.current.indexOf(featureUid) !== -1) {
+      setIsSelected(true);
+      setOpen(true);
+    } else {
+      setIsSelected(false);
+      setOpen(false);
+    }
+  }, [featureUid, selectedFeatures]);
+
   function handleZoomIn(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
     e.stopPropagation();
     api.map(mapId).zoomToExtent(feature.extent);
     setOpen(true);
+  }
+
+  function handleSelect(e: React.MouseEvent<HTMLLIElement, MouseEvent>) {
+    e.stopPropagation();
+    if (!isSelected) {
+      setIsSelected(true);
+      api.event.emit(featureHighlightPayload(EVENT_NAMES.FEATURE_HIGHLIGHT.EVENT_HIGHLIGHT_FEATURE, mapId, feature));
+    } else {
+      setIsSelected(false);
+      api.event.emit(clearHighlightsPayload(EVENT_NAMES.FEATURE_HIGHLIGHT.EVENT_HIGHLIGHT_CLEAR, mapId, featureUid));
+    }
+  }
+
+  function handleOpen(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.stopPropagation();
+    setOpen(!isOpen);
   }
 
   useEffect(() => {
@@ -151,13 +193,13 @@ export function FeatureInfo(props: TypeFeatureProps): JSX.Element {
             src={item}
             tabIndex={0}
             onClick={() => {
-              setIsLighboxOpen(true);
+              setIsLightboxOpen(true);
               setSlides(slidesSetup);
               setSlidesIndex(index);
             }}
             onKeyDown={(e) => {
               if (e.key === 'Enter') {
-                setIsLighboxOpen(true);
+                setIsLightboxOpen(true);
                 setSlides(slidesSetup);
               }
             }}
@@ -188,15 +230,24 @@ export function FeatureInfo(props: TypeFeatureProps): JSX.Element {
 
   return (
     <>
-      <ListItem sx={sxClasses.layerItem} onClick={() => setOpen(!isOpen)}>
+      <ListItem sx={isSelected ? sxClasses.layerItemSelected : sxClasses.layerItem} onClick={(e) => handleSelect(e)}>
         <ListItemButton>
           <ListItemIcon>
-            <IconButton color="primary">{isOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}</IconButton>
+            <IconButton color="primary" onClick={(e) => handleOpen(e)}>
+              {isOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+            </IconButton>
           </ListItemIcon>
           <ListItemIcon>
             <img alt={nameFieldValue} src={featureIconSrc} style={{ ...theme.iconImg, width: '35px', height: '35px' }} />
           </ListItemIcon>
           <ListItemText sx={sxClasses.itemText} primary={nameFieldValue} />
+          <ListItemIcon>
+            <IconButton color="primary">
+              <Tooltip title={t('details.select')} placement="top" enterDelay={1000}>
+                {isSelected ? <CheckBoxIcon /> : <CheckBoxOutIcon />}
+              </Tooltip>
+            </IconButton>
+          </ListItemIcon>
           <ListItemIcon>
             <IconButton color="primary" onClick={(e) => handleZoomIn(e)}>
               <Tooltip title={t('details.zoom_to')} placement="top" enterDelay={1000}>
@@ -216,7 +267,7 @@ export function FeatureInfo(props: TypeFeatureProps): JSX.Element {
               // TODO: because lighbox element is render outside the map container, the focus trap is not able to access it.
               // TODO: if we use the keyboard to access the image, we can only close with esc key.
               // TODO: #1113
-              setIsLighboxOpen(false);
+              setIsLightboxOpen(false);
               setSlides([]);
             }}
           />
