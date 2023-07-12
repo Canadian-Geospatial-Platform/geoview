c9e46f15dc12a49dcb0a663a1066a69b54b6975a
jolevesq <johann.levesque@nrcan-rncan.gc.ca>
2023-07-12 15:43:50 -0400

    feat(details): Add support for link in details fields info Closes #1108

---
+++ packages/geoview-core/src/ui/card-media/card-media.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/require-default-props */
+import MaterialCardMedia from '@mui/material/CardMedia';
+import { CardMediaProps } from '@mui/material';
+
+/**
+ * Properties for the Card Media
+ */
+interface TypeCardMediaProps extends CardMediaProps {
+  alt: string;
+  click?(): void;
+  keyDown?(e: unknown): void;
+}
+
+/**
+ * Create a customized Material UI Card Media
+ *
+ * @param {TypeCardMediaProps} props the properties passed to the Card Media element
+ * @returns {JSX.Element} the created Card Media element
+ */
+export function CardMedia(props: TypeCardMediaProps): JSX.Element {
+  const { sx, src, alt, click, keyDown } = props;
+
+  return <MaterialCardMedia component="img" sx={sx} alt={alt} src={src} tabIndex={0} onClick={click} onKeyDown={keyDown} />;
+}
