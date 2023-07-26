a51d0135080a5dea76719acf714cf6e86a156da5
Alex-NRCan <alexandre.roy@nrcan-rncan.gc.ca>
2023-07-26 16:15:33 -0400

    New linear-progress UI component

---
+++ packages/geoview-core/src/ui/linear-progress/linear-progress.tsx
@@ -0,0 +1,41 @@
+import { LinearProgress as LinearProgressBar } from '@mui/material';
+
+/**
+ * Properties for the Accordion element
+ */
+interface ProgressbarProps {
+  className: string;
+  /**
+   * The variant to use.
+   */
+  variant?: 'determinate' | 'indeterminate' | 'buffer' | 'query';
+  /**
+   * The value of the progress indicator for the determinate and buffer variants.
+   * Value between 0 and 100.
+   */
+  value?: number;
+}
+
+/**
+ * Create a customized Progress bar UI
+ *
+ * @param {ProgressbarProps} props the properties passed to the element
+ * @returns {JSX.Element} the created element
+ */
+export function ProgressBar(props: ProgressbarProps): JSX.Element {
+  const { className, variant, value } = props;
+  return <LinearProgressBar variant={variant} value={value} className={className} />;
+}
+
+/**
+ * Default property values
+ */
+ProgressBar.defaultProps = {
+  variant: 'indeterminate',
+  value: 0,
+};
+
+/**
+ * Example of usage by application code
+ * <ProgressBar variant='determinate' value={progress}></ProgressBar>
+ */
