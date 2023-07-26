9ddf835e66dfaba2f2b2c9da8ed782fc4120110e
kaminderpal <kaminderpal@yahoo.com>
2023-07-26 16:08:30 -0400

    map based on layers of map

--- packages/geoview-core/src/core/components/data-table/data-table-api.ts
+++ packages/geoview-core/src/core/components/data-table/data-table-api.ts
@@ -1,5 +1,16 @@
-import { createElement } from 'react';
+import { createElement, ReactElement } from 'react';
 import DataTable, { DataTableData } from './data-table';
+
+import {
+  AbstractGeoViewVector,
+  api,
+  TypeLayerDataGridProps,
+  TypeListOfLayerEntryConfig,
+  isVectorLayer,
+  TypeArrayOfFeatureInfoEntries,
+  TypeFieldEntry,
+} from '@/app';
+import MapDataTable from './map-data-table';
 
 export class DataTableApi {
   mapId!: string;
@@ -21,5 +32,97 @@ export class DataTableApi {
 
   createDataTable = ({ data }: { data: DataTableData }) => {
     return createElement(DataTable, { data }, []);
+  };
+
+  /**
+   * Create group layer keys based on layer rendered on map
+   *
+   * @param listOfLayerEntryConfig list of layers configured to be rendered on map.
+   * @param parentLayerId layer id
+   * @param grouplayerKeys list of keys already exists.
+   * @returns array of layer keys
+   */
+
+  getGroupKeys = (listOfLayerEntryConfig: TypeListOfLayerEntryConfig, parentLayerId: string, grouplayerKeys: string[]) => {
+    listOfLayerEntryConfig.forEach((LayerEntryConfig) => {
+      if (
+        LayerEntryConfig.entryType === 'group' &&
+        LayerEntryConfig.listOfLayerEntryConfig !== undefined &&
+        LayerEntryConfig.listOfLayerEntryConfig.length > 1
+      ) {
+        this.getGroupKeys(LayerEntryConfig.listOfLayerEntryConfig, `${parentLayerId}/${LayerEntryConfig.layerId}`, grouplayerKeys);
+      } else if (LayerEntryConfig.entryType !== 'group') {
+        grouplayerKeys.push(`${parentLayerId}/${LayerEntryConfig.layerId}`);
+      }
+    });
+    return grouplayerKeys;
+  };
+
+  /**
+   * Create a data table rows
+   *
+   * @param {arrayOfFeatureInfoEntries} arrayOfFeatureInfoEntries the properties of the data table to be created
+   * @return {TypeJsonArray} the data table rows
+   */
+
+  buildFeatureRows = (arrayOfFeatureInfoEntries: TypeArrayOfFeatureInfoEntries) => {
+    const rows = arrayOfFeatureInfoEntries.map((entries) => {
+      return Object.values(entries.fieldInfo).reduce((acc, curr) => {
+        if (curr) {
+          acc[curr.alias] = curr.value as string;
+        }
+
+        return acc;
+      }, {} as Record<string, string>);
+    });
+
+    const columns = arrayOfFeatureInfoEntries.reduce((acc, curr) => {
+      const entries = Object.values(curr.fieldInfo) as TypeFieldEntry[];
+      entries.forEach((entry) => {
+        if (entry) {
+          acc[entry.alias] = entry.alias;
+        }
+      });
+      return acc;
+    }, {} as Record<string, string>);
+
+    return {
+      features: rows,
+      fieldAliases: columns,
+    };
+  };
+
+  /**
+   * Create data table based on layer id from map.
+   * @param {string} layerId layerId of the feature added on map.
+   * @returns Promise of ReactElement.
+   */
+
+  createDataTableByLayerId = async ({ layerId }: TypeLayerDataGridProps): Promise<ReactElement | null> => {
+    const geoviewLayerInstance = api.map(this.mapId).layer.geoviewLayers[layerId];
+
+    if (
+      geoviewLayerInstance.listOfLayerEntryConfig.length > 0 &&
+      (geoviewLayerInstance as AbstractGeoViewVector).getAllFeatureInfo !== undefined
+    ) {
+      const groupLayerKeys = this.getGroupKeys(geoviewLayerInstance.listOfLayerEntryConfig, layerId, []);
+
+      if (isVectorLayer(geoviewLayerInstance)) {
+        const requests = groupLayerKeys.map((layerKey) => {
+          return (geoviewLayerInstance as AbstractGeoViewVector)?.getAllFeatureInfo(layerKey);
+        });
+
+        const response = await Promise.allSettled(requests);
+        const data = response
+          .filter((req) => req.status === 'fulfilled')
+          .map((result) => {
+            /* @ts-expect-error value prop is part of promise, filter function already filter fullfilled promise, still thrown type error. */
+            return this.buildFeatureRows(result.value);
+          });
+
+        return createElement(MapDataTable, { data: data[0] }, []);
+      }
+    }
+    return null;
   };
 }
---
+++ packages/geoview-core/src/core/components/data-table/json-export-button.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { IconButton, DownloadIcon, Tooltip } from '../../../ui';
+
+interface JSONExportButtonProps {
+  features: Record<string, string>[];
+}
+
+/**
+ * Custom  export button which will help to download data table data in csv format.
+ * @param {ColumnsType} dataTableData list of rows to be displayed in data table
+ * @param {MRTColumnDef<ColumnsType>[]} columns array of object represent column header data.
+ * @returns {JSX.Element} returns export button
+ *
+ */
+function JSONExportButton({ features }: JSONExportButtonProps): JSX.Element {
+  const { t } = useTranslation<string>();
+
+  /**
+   * build the JSON file
+   * @return {JSON.stringify} Json gile content
+   *
+   */
+  const getJson = () => {
+    // Stringify with some indentation
+    return JSON.stringify(features, null, 2);
+  };
+
+  /**
+   * export the blob to a file
+   * @param {Blob} blob the blob to save to file
+   * @param {string} filename file name
+   */
+  const exportBlob = (blob: Blob, filename: string) => {
+    // Save the blob in a json file
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    a.click();
+
+    URL.revokeObjectURL(url);
+  };
+
+  /**
+   * Export data table in csv format.
+   */
+  const handleExportData = () => {
+    const jsonString = getJson();
+    const blob = new Blob([jsonString], {
+      type: 'text/json',
+    });
+
+    exportBlob(blob, `table.json`);
+  };
+
+  return (
+    <IconButton onClick={handleExportData}>
+      <Tooltip title={t('dataTable.jsonExportBtn')} placement="bottom" enterDelay={100}>
+        <DownloadIcon />
+      </Tooltip>
+    </IconButton>
+  );
+}
+
+export default JSONExportButton;
