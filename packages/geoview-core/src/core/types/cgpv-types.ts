ef59dec190962a549e3014e8e710e5554836b368
cphelefu <cphelefu@gmail.com>
2023-07-27 14:42:18 -0400

    fix to error

--- packages/geoview-core/src/core/types/cgpv-types.ts
+++ packages/geoview-core/src/core/types/cgpv-types.ts
@@ -1,6 +1,7 @@
 export * from './global-types';
 export * from './marker-definitions';
 export * from '../app-start';
+
 export * from '../containers/focus-trap';
 export * from '../containers/html-to-react';
 export * from '../containers/shell';
@@ -9,10 +10,12 @@ export * from '../utils/config/config';
 export * from '../utils/constant';
 export * from '../utils/date-mgt';
 export * from '../utils/utilities';
-export * from '../../app';
-export * from '@/core/components';
-export * from '@/geo';
+
 export * from '@/api';
+
+export * from '@/core/components';
+export * from '@/app';
+export * from '@/geo';
 export * from '@/ui';
 
 export type { AnySchemaObject } from 'ajv';
