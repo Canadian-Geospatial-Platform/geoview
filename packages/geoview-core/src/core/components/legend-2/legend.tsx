f4e53b99e62d94f79e931e9efad67167dfdec441
cphelefu <cphelefu@gmail.com>
2023-09-15 14:07:52 -0400

    fixing as per code reviews

--- packages/geoview-core/src/core/components/legend-2/legend.tsx
+++ packages/geoview-core/src/core/components/legend-2/legend.tsx
@@ -16,65 +16,6 @@ export function Legend2(props: LegendItemsDetailsProps): JSX.Element {
   const { layerIds, isRemoveable, canSetOpacity, expandAll, hideAll, mapId } = props;
   api.event.emit({ handlerName: `${mapId}/$LegendsLayerSet$`, event: api.eventNames.GET_LEGENDS.TRIGGER });
 
-  // const [selectedMainLayer, setSelectedMainLayer] = React.useState<string | null>(null);
-  // console.log(selectedMainLayer);
-  // const handleMainLayerClick = (layerId: string) => {
-  //   setSelectedMainLayer(layerId);
-  // };
-
-  //   const leftPanel = () => {
-  //     return (
-  //       <LegendItems
-  //         mapId={mapId}
-  //         layerIds={layerIds}
-  //         canSetOpacity={canSetOpacity}
-  //         expandAll={expandAll}
-  //         hideAll={hideAll}
-  //         isRemoveable={isRemoveable}
-  //         canZoomTo
-  //         // onMainLayerClick={handleMainLayerClick}
-  //       />
-  //     );
-  //   };
-
-  //   const rightPanel = () => {
-  //     // Condition to render sublayers in the right panel when a main layer is selected
-  //     if (selectedMainLayer) {
-  //       return (
-  //         <LegendItemsDetails
-  //           mapId={mapId}
-  //           layerIds={[selectedMainLayer]}
-  //           canSetOpacity={canSetOpacity}
-  //           expandAll={expandAll}
-  //           hideAll={hideAll}
-  //           isRemoveable={isRemoveable}
-  //           canZoomTo
-  //           // subLayerId={subLayerId}
-  //         />
-  //       );
-  //     } else {
-  //       return (
-  //         <Item>
-  //           <p>This will be the right panel eventually</p>
-  //         </Item>
-  //       );
-  //     }
-  //   };
-
-  //   return (
-  //     <Box sx={{ px: '20px', pb: '20px', display: 'flex', flexDirection: 'column' }}>
-  //       <Grid container direction={{ xs: 'column', sm: 'row' }} spacing={{ xs: 1, sm: 4, md: 8 }}>
-  //         <Grid item xs={12} sm={4}>
-  //           <Item>{leftPanel()}</Item>
-  //         </Grid>
-  //         <Grid item xs={12} sm={6}>
-  //           <Item>{rightPanel()}</Item>
-  //         </Grid>
-  //       </Grid>
-  //     </Box>
-  //   );
-  // }
-
   const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
 
   const leftPanel = () => {
