6be459d5f0065fd2be08697ce690ba54ce1fc3e8
Christopher Phelefu <cphelefu@gmail.com>
2023-10-02 12:16:35 -0400

    added buttons, fixed tables, fixed bug, fixed opacity, translations and style

--- packages/geoview-core/src/core/components/legend-2/legend.tsx
+++ packages/geoview-core/src/core/components/legend-2/legend.tsx
@@ -1,10 +1,10 @@
-import { Button, FormControlLabel, FormGroup, styled, useTheme } from '@mui/material';
-import React, { useState } from 'react';
+import { Button, styled, useTheme } from '@mui/material';
+import React, { useState, useEffect } from 'react';
 import { useStore } from 'zustand';
 import { useTranslation } from 'react-i18next';
 import { api } from '@/app';
 import { LegendItemsDetailsProps } from './types';
-import { AddIcon, Box, Checkbox, Grid, List, Typography, ExpandMoreIcon } from '@/ui';
+import { AddIcon, Box, Grid, List, Typography, ExpandMoreIcon, Paper, Stack, ExpandIcon } from '@/ui';
 import { LegendItemDetails } from './legend-item-details/legend-item-details';
 import { getGeoViewStore } from '@/core/stores/stores-managers';
 import { LegendItem } from './legend-item';
@@ -34,8 +34,18 @@ export function Legend2(props: LegendItemsDetailsProps): JSX.Element {
     },
     legendTitle: {
       textAlign: 'left',
-      font: theme.footerPanel.titleFont,
-      marginBottom: '10px',
+      fontFamily: 'Open Sans, Semibold',
+      fontSize: '18px',
+    },
+    categoryTitle: {
+      textAlign: 'left',
+      fontFamily: 'Open Sans, Semibold',
+      fontSize: '20px',
+    },
+    legendButtonText: {
+      fontFamily: 'Open Sans, Semibold',
+      color: '#515BA5',
+      fontSize: '16px',
     },
   };
 
@@ -43,24 +53,55 @@ export function Legend2(props: LegendItemsDetailsProps): JSX.Element {
   const selectedLegendItem = useStore(store, (state) => state.legendState.selectedItem);
   const [isSelectedLayersClicked, setIsSelectedLayersClicked] = useState(false);
 
-  const onSelectedLayersClick = () => {
-    setIsSelectedLayersClicked(!isSelectedLayersClicked);
-  };
-
   function showSelectedLayersPanel() {
     return (
-      <Box
-        onClick={onSelectedLayersClick}
+      <Paper
+        onClick={() => setIsSelectedLayersClicked(!isSelectedLayersClicked)}
         sx={{
+          justifyContent: 'space-between',
+          padding: '9px 17px 10px 57px',
+          alignItems: 'center',
+          marginBottom: '27px',
           display: 'flex',
           flexDirection: 'row',
-          justifyContent: 'space-between',
-          margin: '20px',
           cursor: 'pointer',
         }}
       >
-        {t('legend.selected_Layers')}
-        <ExpandMoreIcon sx={{ transform: 'rotate(270deg)' }} />
+        <div>
+          <Typography sx={sxClasses.legendTitle}>{t('legend.overview_title')}</Typography>
+          <Typography sx={{ fontSize: '0.6em' }}>{t('legend.selected_Layers')}</Typography>
+        </div>
+        <div>
+          <ExpandMoreIcon sx={{ transform: 'rotate(270deg)' }} />
+        </div>
+      </Paper>
+    );
+  }
+
+  function buttonsMenu() {
+    return (
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '15px' }}>
+        <div>
+          <Typography sx={sxClasses.categoryTitle}>{t('legend.categories_title')}</Typography>
+        </div>
+        <Stack style={{ alignItems: 'center', gap: '15px' }} direction="row">
+          <Button
+            variant="contained"
+            size="small"
+            sx={{ backgroundColor: '#F4F5FF' }}
+            startIcon={<ExpandIcon fontSize="small" sx={{ color: '#515BA5' }} />}
+          >
+            <Typography sx={sxClasses.legendButtonText}>{t('legend.re-arrange')}</Typography>
+          </Button>
+          <Button
+            variant="contained"
+            size="small"
+            sx={{ backgroundColor: '#F4F5FF' }}
+            startIcon={<AddIcon fontSize="small" sx={{ color: '#515BA5' }} />}
+          >
+            <Typography sx={sxClasses.legendButtonText}>{t('legend.add_layer')}</Typography>
+          </Button>
+        </Stack>
       </Box>
     );
   }
@@ -87,10 +128,17 @@ export function Legend2(props: LegendItemsDetailsProps): JSX.Element {
     return (
       <div>
         {showSelectedLayersPanel()}
+        {buttonsMenu()}
         <List sx={{ width: '100%' }}>{legendItems}</List>
       </div>
     );
   };
+
+  useEffect(() => {
+    if (selectedLegendItem) {
+      setIsSelectedLayersClicked(false);
+    }
+  }, [selectedLegendItem]);
 
   function rightPanel() {
     if (isSelectedLayersClicked) {
@@ -98,20 +146,17 @@ export function Legend2(props: LegendItemsDetailsProps): JSX.Element {
     }
     if (selectedLegendItem) {
       return (
-        <>
-          <Typography sx={sxClasses.legendTitle}>{t('legend.selected_legend')}</Typography>
-          <Item>
-            <LegendItemDetails
-              key={`layerKey-${selectedLegendItem.layerId}`}
-              layerId={selectedLegendItem.layerId}
-              geoviewLayerInstance={selectedLegendItem?.geoviewLayerInstance}
-              isRemoveable={selectedLegendItem.isRemoveable}
-              canSetOpacity={selectedLegendItem.canSetOpacity}
-              expandAll={selectedLegendItem.expandAll}
-              hideAll={selectedLegendItem?.hideAll}
-            />
-          </Item>
-        </>
+        <Item>
+          <LegendItemDetails
+            key={`layerKey-${selectedLegendItem.layerId}`}
+            layerId={selectedLegendItem.layerId}
+            geoviewLayerInstance={selectedLegendItem?.geoviewLayerInstance}
+            isRemoveable={selectedLegendItem.isRemoveable}
+            canSetOpacity={selectedLegendItem.canSetOpacity}
+            expandAll={selectedLegendItem.expandAll}
+            hideAll={selectedLegendItem?.hideAll}
+          />
+        </Item>
       );
     }
 
@@ -122,17 +167,7 @@ export function Legend2(props: LegendItemsDetailsProps): JSX.Element {
     <Box sx={sxClasses.legendContainer}>
       <Grid container direction={{ xs: 'column', sm: 'row' }} spacing={{ xs: 1, sm: 4, md: 8 }}>
         <Grid item xs={12} sm={6}>
-          <Typography sx={sxClasses.legendTitle}>{t('legend.overview_title')}</Typography>
-
-          <FormGroup sx={{ fontSize: '0.8em' }}>
-            <FormControlLabel control={<Checkbox color="primary" size="small" />} label={t('legend.sort_layers')} />
-          </FormGroup>
-
           {leftPanel()}
-
-          <Button sx={{ marginTop: '20px' }} variant="contained" size="small" color="primary" startIcon={<AddIcon fontSize="small" />}>
-            {t('legend.add_layer')}
-          </Button>
         </Grid>
         <Grid item xs={12} sm={6}>
           {rightPanel()}
