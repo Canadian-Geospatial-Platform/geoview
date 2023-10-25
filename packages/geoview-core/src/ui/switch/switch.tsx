284e658c5ff907b64336b1adb35fa429ef65f972
kaminderpal <kaminderpal@yahoo.com>
2023-10-24 21:40:03 -0400

    fix(Switch): mui styles refactor  Closes #646

--- packages/geoview-core/src/ui/switch/switch.tsx
+++ packages/geoview-core/src/ui/switch/switch.tsx
@@ -1,29 +1,11 @@
-import { FormControlLabel as MaterialFormControlLabel, Switch as MaterialSwitch, SwitchProps } from '@mui/material';
-
-import makeStyles from '@mui/styles/makeStyles';
+import { FormControlLabel as MaterialFormControlLabel, Switch as MaterialSwitch, SwitchProps, useTheme } from '@mui/material';
+import { getSxClasses } from './switch-style';
 
 /**
  * Custom Material UI Switch properties
  */
 // eslint-disable-next-line react/require-default-props
 export type TypeSwitchProps = SwitchProps & { mapId?: string };
-
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    width: '100%',
-    marginRight: '5px',
-    marginLeft: '5px',
-    '& .MuiSwitch-switchBase.Mui-focusVisible': {
-      color: theme.palette.primary.contrastText,
-      background: theme.palette.primary.light,
-    },
-    '& .MuiFormControlLabel-label': {
-      fontSize: theme.typography.fontSize,
-      color: theme.palette.primary.light,
-      whiteSpace: 'nowrap',
-    },
-  },
-}));
 
 /**
  * Create a Material UI Swich component
@@ -32,9 +14,10 @@ const useStyles = makeStyles((theme) => ({
  * @returns {JSX.Element} the switch ui component
  */
 export function Switch(props: TypeSwitchProps): JSX.Element {
-  const classes = useStyles();
+  const sxtheme = useTheme();
+  const classes = getSxClasses(sxtheme);
 
   const { title, ...otherProps } = props;
 
-  return <MaterialFormControlLabel control={<MaterialSwitch {...otherProps} />} label={title} className={classes.formControl} />;
+  return <MaterialFormControlLabel control={<MaterialSwitch {...otherProps} />} label={title} sx={classes.formControl} />;
 }
