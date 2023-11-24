39abf96abf93fac2f79b56d4d9fa443e1371cf36
Christopher Phelefu <cphelefu@gmail.com>
2023-11-24 13:44:39 -0500

    fix tooltips

--- packages/geoview-core/src/core/components/layers/left-panel/layers-actions.tsx
+++ packages/geoview-core/src/core/components/layers/left-panel/layers-actions.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { Button } from '@mui/material';
-import { Box, AddCircleOutlineIcon, ButtonGroup, DeleteOutlineIcon, HandleIcon } from '@/ui';
+import { Box, AddCircleOutlineIcon, ButtonGroup, DeleteOutlineIcon, HandleIcon, Tooltip } from '@/ui';
 import { useLayerStoreActions, useLayersDisplayState } from '@/core/stores/store-interface-and-intial-values/layer-state';
 import { TypeLayersViewDisplayState } from '../types';
 
@@ -18,27 +18,33 @@ export function LayersActions(): JSX.Element {
   return (
     <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'right', marginBottom: '15px' }}>
       <ButtonGroup size="small" variant="outlined" aria-label="outlined button group">
-        <Button
-          variant={displayState === 'add' ? 'contained' : 'outlined'}
-          startIcon={<AddCircleOutlineIcon fontSize="small" />}
-          onClick={() => handleSetDisplayState('add')}
-        >
-          {t('general.add')}
-        </Button>
-        <Button
-          variant={displayState === 'order' ? 'contained' : 'outlined'}
-          startIcon={<HandleIcon fontSize="small" />}
-          onClick={() => handleSetDisplayState('order')}
-        >
-          {t('legend.re-arrange')}
-        </Button>
-        <Button
-          variant={displayState === 'remove' ? 'contained' : 'outlined'}
-          startIcon={<DeleteOutlineIcon fontSize="small" />}
-          onClick={() => handleSetDisplayState('remove')}
-        >
-          {t('general.remove')}
-        </Button>
+        <Tooltip title={t('legend.add_layer')} placement="top" enterDelay={1000}>
+          <Button
+            variant={displayState === 'add' ? 'contained' : 'outlined'}
+            startIcon={<AddCircleOutlineIcon fontSize="small" />}
+            onClick={() => handleSetDisplayState('add')}
+          >
+            {t('general.add')}
+          </Button>
+        </Tooltip>
+        <Tooltip title={t('legend.sort_layers')} placement="top" enterDelay={1000}>
+          <Button
+            variant={displayState === 'order' ? 'contained' : 'outlined'}
+            startIcon={<HandleIcon fontSize="small" />}
+            onClick={() => handleSetDisplayState('order')}
+          >
+            {t('legend.sort')}
+          </Button>
+        </Tooltip>
+        <Tooltip title={t('legend.remove_layer')} placement="top" enterDelay={1000}>
+          <Button
+            variant={displayState === 'remove' ? 'contained' : 'outlined'}
+            startIcon={<DeleteOutlineIcon fontSize="small" />}
+            onClick={() => handleSetDisplayState('remove')}
+          >
+            {t('general.remove')}
+          </Button>
+        </Tooltip>
       </ButtonGroup>
     </Box>
   );
