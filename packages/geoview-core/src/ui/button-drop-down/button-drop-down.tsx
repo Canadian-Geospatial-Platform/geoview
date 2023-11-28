e8ae2a3288a6152a2bcf65c92f1b10dc5db8f125
Alex <alexandre.roy@nrcan-rncan.gc.ca>
2023-11-28 09:06:28 -0500

    Added a new button-drop-down ui component which acts like a button with a drop down choice next to it to switch the actual button action

---
+++ packages/geoview-core/src/ui/button-drop-down/button-drop-down.tsx
@@ -0,0 +1,118 @@
+import { useRef, useState } from 'react';
+import { ButtonGroupProps, ClickAwayListener, Grow, MenuItem, MenuList, Paper, Popper } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { Button } from '../button/button';
+import { ButtonGroup } from '../button-group/button-group';
+import { getSxClasses } from './button-drop-down-style';
+import { ArrowDownIcon } from '../icons';
+
+/**
+ * The ButtonDropDown props
+ */
+export type ButtonDropDownProps = ButtonGroupProps & { options: string[]; onButtonClick: (index: number, text: string) => void };
+
+/**
+ * Create a customized Material UI Button Drop Down
+ *
+ * @param {ButtonDropDownProps} props the properties passed to the Button Drop Down element
+ * @returns {JSX.Element} the created Button Drop Down element
+ */
+export function ButtonDropDown(props: ButtonDropDownProps): JSX.Element {
+  const { options, onButtonClick } = props;
+
+  const theme = useTheme();
+  const sxClasses = getSxClasses(theme);
+
+  /** ****************************************** USE STATE SECTION START ************************************************ */
+
+  const [open, setOpen] = useState<boolean>(false);
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
+  const anchorRef = useRef<HTMLDivElement>(null);
+
+  /** ****************************************** USE STATE SECTION END ************************************************** */
+  /** *************************************** EVENT HANDLERS SECTION START ********************************************** */
+
+  /**
+   * Handles a click on the button itself
+   */
+  const handleClick = (): void => {
+    // Callback
+    onButtonClick?.(selectedIndex, `${options[selectedIndex]}`);
+  };
+
+  /**
+   * Toggles the open state of the drop down
+   */
+  const handleToggle = (): void => {
+    // Toggle the open state
+    setOpen(!open);
+  };
+
+  /**
+   * Handles a click in an item in the drop down menu
+   */
+  const handleMenuItemClick = (event: React.MouseEvent<HTMLLIElement, MouseEvent>, index: number) => {
+    setSelectedIndex(index);
+    setOpen(false);
+  };
+
+  /**
+   * Handles a when the user clicks away of the drop down
+   */
+  const handleClickAway = (event: Event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target as HTMLElement)) {
+      return;
+    }
+    setOpen(false);
+  };
+
+  /** **************************************** EVENT HANDLERS SECTION END *********************************************** */
+  /** ******************************************** RENDER SECTION START ************************************************* */
+
+  // Renders
+  return (
+    <>
+      <ButtonGroup {...props} sx={sxClasses.buttonDropDown} ref={anchorRef}>
+        <Button sx={sxClasses.buttonText} type="text" onClick={handleClick}>
+          {options[selectedIndex]}
+        </Button>
+        <Button sx={sxClasses.buttonArrow} type="icon" size="small" onClick={handleToggle}>
+          <ArrowDownIcon />
+        </Button>
+      </ButtonGroup>
+      <Popper
+        sx={{
+          zIndex: 1,
+        }}
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClickAway}>
+                <MenuList id="split-button-menu" autoFocusItem>
+                  {options.map((option, index) => (
+                    <MenuItem key={option} selected={index === selectedIndex} onClick={(event) => handleMenuItemClick(event, index)}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </>
+  );
+
+  /** ******************************************** RENDER SECTION END ************************************************* */
+}
