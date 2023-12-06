f5bebc3acedda08377dc63ebae91d9d5901590e5
jolevesq <johann.levesque@nrcan-rncan.gc.ca>
2023-12-06 16:40:45 -0500

    refactor(core): Refactor config loadin and store usage to set default Closes #1569-store-config

--- packages/geoview-core/src/ui/style/theme.tsx
+++ packages/geoview-core/src/ui/style/theme.tsx
@@ -1,7 +1,8 @@
 import { createTheme } from '@mui/material/styles';
 import { lightThemeOptions } from './light-theme';
 import { darkThemeOptions } from './dark-theme';
-import { royalThemeOptions } from './royal-theme';
+import { geoCaThemeOptions } from './geo-ca-theme';
+import { TypeSupportedTheme } from '@/geo/map/map-schema-types';
 
 declare module '@mui/material/styles/createPalette' {
   interface Palette {
@@ -11,21 +12,21 @@ declare module '@mui/material/styles/createPalette' {
   }
 }
 
-function getThemeOptions(mode: string | undefined) {
+function getThemeOptions(mode: TypeSupportedTheme) {
   switch (mode) {
     case 'dark':
       return darkThemeOptions;
-    case 'royal':
-      return royalThemeOptions;
+    case 'geo.ca':
+      return geoCaThemeOptions;
     default:
       return lightThemeOptions;
   }
 }
 
-export const getTheme = (mode: 'light' | 'dark' | 'royal' | undefined) => {
+export const getTheme = (mode: TypeSupportedTheme) => {
   const optionClone = getThemeOptions(mode);
 
   return createTheme(optionClone);
 };
 
-export const cgpvTheme = createTheme(getThemeOptions(undefined));
+export const cgpvTheme = createTheme(getThemeOptions('geo.ca'));
