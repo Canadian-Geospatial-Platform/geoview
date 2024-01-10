0a7290edc958e18e22e163b1d0c7823791b9f4c6
Alex <alexandre.roy@nrcan-rncan.gc.ca>
2024-01-10 09:50:21 -0500

    New getLayerConfigAsync function Vector, Dynamic and WMS done-ish Better error handling in Plugin async logic Core functions Log error when unable to calculate bounds on layer applyViewFilter createMapFromConfig fixes Added async when setting projection and showing circular progress Removed timeout in default-config.html following earlier fix with createMapFromConfig New "Loading UI..." temporary indicator to test data table and legend panels loading speeds Cleanup

--- packages/geoview-core/src/api/event-processors/event-processor-children/legend-event-processor.ts
+++ packages/geoview-core/src/api/event-processors/event-processor-children/legend-event-processor.ts
@@ -190,7 +190,8 @@ export class LegendEventProcessor extends AbstractEventProcessor {
         try {
           const myLayer = await api.maps[mapId].layer.getGeoviewLayerByIdAsync(layerPathNodes[0], true);
           newLegendLayer.bounds = myLayer?.calculateBounds(layerPath);
-        } catch {
+        } catch (error) {
+          console.error(`Couldn't calculate bounds on layer ${layerPath}`);
           newLegendLayer.bounds = undefined;
         }
       }
