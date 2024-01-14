003c668b816389e54b9aa78f2dce948d8d67405e
kaminderpal <kaminderpal@yahoo.com>
2024-01-14 18:29:27 -0500

    fix(footerTabs): fix legend fullscreen #closes1616

--- packages/geoview-core/src/core/components/legend/legend.tsx
+++ packages/geoview-core/src/core/components/legend/legend.tsx
@@ -1,8 +1,14 @@
 import { useTheme } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Box, Typography } from '@/ui';
-import { useMapVisibleLayers, useLayerStoreActions } from '@/core/stores/';
+import {
+  useMapVisibleLayers,
+  useLayerStoreActions,
+  useAppFullscreenActive,
+  useUIActiveFooterTabId,
+  useUIFooterPanelResizeValue,
+} from '@/core/stores/';
 import { getSxClasses } from './legend-styles';
 import { LegendLayer } from './legend-layer';
 import { TypeLegendLayer } from '../layers/types';
@@ -16,6 +22,12 @@ export function Legend(): JSX.Element {
 
   const visibleLayers = useMapVisibleLayers();
   const { getLayer } = useLayerStoreActions();
+
+  const legendPanelRef = useRef<HTMLDivElement>(null);
+
+  const isMapFullScreen = useAppFullscreenActive();
+  const footerPanelResizeValue = useUIFooterPanelResizeValue();
+  const activeFooterTabId = useUIActiveFooterTabId();
 
   useEffect(() => {
     const parentPaths: string[] = [];
@@ -33,9 +45,22 @@ export function Legend(): JSX.Element {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [visibleLayers]);
 
+  useEffect(() => {
+    if (isMapFullScreen && legendPanelRef.current && activeFooterTabId === 'legend') {
+      const tabsContainer = document.getElementById('tabsContainer')!;
+      const firstChild = tabsContainer.firstElementChild?.firstElementChild;
+      const firstChildHeight = firstChild?.clientHeight || 0;
+      const leftPanelHeight = (window.screen.height * footerPanelResizeValue) / 100 - firstChildHeight;
+
+      legendPanelRef.current.style.maxHeight = `${leftPanelHeight}px`;
+      legendPanelRef.current.style.overflow = `auto`;
+      legendPanelRef.current.style.paddingBottom = `24px`;
+    }
+  }, [footerPanelResizeValue, isMapFullScreen, activeFooterTabId]);
+
   function renderLegendLayersList() {
     return (
-      <Box display="flex" flexDirection="row" flexWrap="wrap" style={{ overflow: 'auto' }}>
+      <Box display="flex" flexDirection="row" flexWrap="wrap" style={!isMapFullScreen ? { overflow: 'auto' } : {}}>
         {legendLayers.map((item) => (
           <Box key={item!.layerPath} width={{ xs: '100%', sm: '50%', md: '33.33%', lg: '25%', xl: '25%' }} style={{ minHeight: 0 }} p={2}>
             <LegendLayer layer={item!} key={item!.layerPath} />
@@ -46,7 +71,7 @@ export function Legend(): JSX.Element {
   }
 
   return (
-    <Box sx={sxClasses.container}>
+    <Box sx={sxClasses.container} ref={legendPanelRef}>
       <Box>
         <Typography sx={sxClasses.title}>{t('legend.overviewTitle')}</Typography>
         <Typography sx={sxClasses.subtitle} />
