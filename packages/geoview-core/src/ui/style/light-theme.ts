6487f229b208e7886a71299bffd55e20c5a4a814
Christopher Phelefu <cphelefu@gmail.com>
2024-02-13 15:06:49 -0500

    fix bg color

--- packages/geoview-core/src/ui/style/light-theme.ts
+++ packages/geoview-core/src/ui/style/light-theme.ts
@@ -4,7 +4,7 @@ import { GeoViewColorClass, IGeoViewColors } from './types';
 export const lightThemeColors: IGeoViewColors = {
   ...defaultGeoViewColors,
 
-  bgColor: new GeoViewColorClass('#F8F8F8'),
+  bgColor: new GeoViewColorClass('#F9F9F9'),
   primary: new GeoViewColorClass('#AA4A44'),
   textColor: new GeoViewColorClass('#000000'),
 };
