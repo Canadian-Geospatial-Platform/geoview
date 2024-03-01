0052bdc191cead242fd68be8d5f14b28f7678cdc
Christopher Phelefu <cphelefu@gmail.com>
2024-03-01 15:18:32 -0500

    fix error

--- packages/geoview-core/src/core/utils/useSpringAnimations.ts
+++ packages/geoview-core/src/core/utils/useSpringAnimations.ts
@@ -2,9 +2,11 @@ import { useSpring, SpringConfig } from '@react-spring/web';
 
 // Common easing function
 const commonConfig: SpringConfig = { duration: 500, easing: (t) => t };
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type SpringValues = Record<string, any>;
 
 // Fade In animation
-export const useFadeIn = (config?: SpringConfig) => {
+export const useFadeIn = (config?: SpringConfig): SpringValues => {
   return useSpring({
     config: { ...commonConfig, ...config },
     from: { opacity: 0 },
@@ -13,7 +15,7 @@ export const useFadeIn = (config?: SpringConfig) => {
 };
 
 // Slide In animation
-export const useSlideIn = (config?: SpringConfig) => {
+export const useSlideIn = (config?: SpringConfig): SpringValues => {
   return useSpring({
     config: { ...commonConfig, ...config },
     from: { transform: 'translateX(-100%)' },
@@ -22,7 +24,7 @@ export const useSlideIn = (config?: SpringConfig) => {
 };
 
 // Scale In animation
-export const useScaleIn = (config?: SpringConfig) => {
+export const useScaleIn = (config?: SpringConfig): SpringValues => {
   return useSpring({
     config: { ...commonConfig, ...config },
     from: { transform: 'scale(0)' },
@@ -31,7 +33,7 @@ export const useScaleIn = (config?: SpringConfig) => {
 };
 
 // Rotate animation
-export const useRotate = (config?: SpringConfig) => {
+export const useRotate = (config?: SpringConfig): SpringValues => {
   return useSpring({
     config: { ...commonConfig, ...config },
     from: { transform: 'rotate(0deg)' },
@@ -40,7 +42,7 @@ export const useRotate = (config?: SpringConfig) => {
 };
 
 // Color Change animation
-export const useColorChange = (config?: SpringConfig) => {
+export const useColorChange = (config?: SpringConfig): SpringValues => {
   return useSpring({
     config: { ...commonConfig, ...config },
     from: { backgroundColor: 'red' },
