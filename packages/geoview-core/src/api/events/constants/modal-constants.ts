2b006219a609df658c6635ad466f0105f01f5cbe
Alex <94073946+Alex-NRCan@users.noreply.github.com>
2024-03-12 08:12:00 -0400

    refactor(modals) Modal events refactored (#1910)
    * Modal events refactored
    
    * Rebased

--- packages/geoview-core/src/api/events/constants/modal-constants.ts
+++ packages/geoview-core/src/api/events/constants/modal-constants.ts
@@ -6,15 +6,10 @@ import { EventStringId } from '../event-types';
  */
 
 /** Valid keys for the MODAL category */
-export type ModalEventKey = 'EVENT_MODAL_CREATE' | 'EVENT_MODAL_OPEN' | 'EVENT_MODAL_CLOSE' | 'EVENT_MODAL_UPDATE';
+export type ModalEventKey = 'EVENT_MODAL_OPEN' | 'EVENT_MODAL_CLOSE';
 
 /** Record that associates MODAL's event keys to their event string id */
 export const MODAL: Record<ModalEventKey, EventStringId> = {
-  /**
-   * Event is triggered when a new modal is created
-   */
-  EVENT_MODAL_CREATE: 'modal/create',
-
   /**
    * Event is triggered when a modal opens
    */
@@ -24,9 +19,4 @@ export const MODAL: Record<ModalEventKey, EventStringId> = {
    * Event is triggered when a modal is closed
    */
   EVENT_MODAL_CLOSE: 'modal/close',
-
-  /**
-   * Event is triggered when a modal is updated
-   */
-  EVENT_MODAL_UPDATE: 'modal/update',
 };
