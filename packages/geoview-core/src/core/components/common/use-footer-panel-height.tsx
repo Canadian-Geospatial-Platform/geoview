a14b67f7e5dd1367e7ade530a7cb0a28e411c498
Christopher Phelefu <cphelefu@gmail.com>
2024-04-25 13:02:29 -0400

    another fix

--- packages/geoview-core/src/core/components/common/use-footer-panel-height.tsx
+++ packages/geoview-core/src/core/components/common/use-footer-panel-height.tsx
@@ -65,7 +65,7 @@ export function useFooterPanelHeight({ footerPanelTab }: UseFooterPanelHeightTyp
       leftPanelRef.current.style.paddingBottom = '24px';
 
       if (activeFooterBarTabId === TABS.DATA_TABLE) {
-        setTableHeight(leftPanelHeight);
+        setTableHeight(leftPanelHeight * 0.9);
         rightPanelHeight(leftPanelHeight);
       } else if (activeFooterBarTabId === TABS.GEO_CHART && rightPanelRef.current) {
         const childElem = rightPanelRef.current?.firstElementChild as HTMLElement | null;
