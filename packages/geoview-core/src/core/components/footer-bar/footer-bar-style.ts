6da07e8be8968cc791d70000bacbb299e221e1c9
Christopher Phelefu <cphelefu@gmail.com>
2024-06-18 15:58:43 -0400

    fix margin using selectors

--- packages/geoview-core/src/core/components/footer-bar/footer-bar-style.ts
+++ packages/geoview-core/src/core/components/footer-bar/footer-bar-style.ts
@@ -20,10 +20,11 @@ export const getSxClasses = (theme: Theme): any => ({
     '& .MuiTabs-indicator': {
       display: 'none',
     },
-    '& .Mui-selected': {
+    '& .MuiTab-root.Mui-selected': {
       color: `${theme.palette.geoViewColor.white} !important`,
       background: theme.palette.geoViewColor.primary.main,
       borderRadius: '0.5rem',
+      margin: '0.5rem',
       minHeight: 0,
     },
   },
