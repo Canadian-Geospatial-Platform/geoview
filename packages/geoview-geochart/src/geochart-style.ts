4374fe2e54ba4dc001141a09b878a78f5178c4ce
Johann Levesque <Johann.Levesque@NRCan-RNCan.gc.ca>
2024-06-26 10:12:04 -0400

    fix right panel tab

--- packages/geoview-geochart/src/geochart-style.ts
+++
@@ -1,6 +0,0 @@
-import { Theme } from '@mui/material/styles';
-
-// ? I doubt we want to define an explicit type for style properties?
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const getSxClasses = (theme: Theme): any => ({
-});
