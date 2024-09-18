7c06901618a5f8395b4635bb64e210dad5f362e1
Yves Choquette <yves.choquette@icloud.com>
2024-09-18 19:29:30 -0400

    2487-Config API for GeoJson

---
+++ packages/geoview-core/src/api/config/types/classes/geoview-config/vector-config/geojson-config.ts
@@ -0,0 +1,185 @@
+import { CV_CONST_LAYER_TYPES, CV_CONST_SUB_LAYER_TYPES, CV_GEOVIEW_SCHEMA_PATH } from '@config/types/config-constants';
+import { AbstractGeoviewLayerConfig } from '@config/types/classes/geoview-config/abstract-geoview-layer-config';
+import { GeoJsonGroupLayerConfig } from '@config/types/classes/sub-layer-config/group-node/geojson-group-layer-config';
+import { TypeJsonArray, TypeJsonObject } from '@config/types/config-types';
+import { TypeDisplayLanguage } from '@config/types/map-schema-types';
+import { GeoJsonLayerEntryConfig } from '@config/types/classes/sub-layer-config/leaf/vector/geojson-layer-entry-config';
+import { EntryConfigBaseClass } from '@config/types/classes/sub-layer-config/entry-config-base-class';
+import { GeoviewLayerConfigError, GeoviewLayerInvalidParameterError } from '@config/types/classes/config-exceptions';
+
+import { logger } from '@/core/utils/logger';
+import { layerEntryIsGroupLayer } from '@config/types/type-guards';
+
+export type TypeGeoJsonLayerNode = GeoJsonGroupLayerConfig | GeoJsonLayerEntryConfig;
+
+// ========================
+// #region CLASS HEADER
+
+/**
+ * The GeoJson geoview layer class.
+ */
+export class GeoJsonLayerConfig extends AbstractGeoviewLayerConfig {
+  // ==================
+  // #region PROPERTIES
+
+  /**
+   * Type of GeoView layer.
+   */
+  override geoviewLayerType = CV_CONST_LAYER_TYPES.GEOJSON;
+
+  /** The layer entries to use from the GeoView layer. */
+  declare listOfLayerEntryConfig: EntryConfigBaseClass[] | TypeGeoJsonLayerNode[];
+  // #endregion PROPERTIES
+
+  // ===============
+  // #region METHODS
+  /*
+   * Methods are listed in the following order: abstract, override, private, protected, public and static.
+   */
+
+  // ================
+  // #region OVERRIDE
+  /**
+   * The getter method that returns the geoview layer schema to use for the validation. Each geoview layer type knows what
+   * section of the schema must be used to do its validation.
+   *
+   * @returns {string} The GeoView layer schema associated to the config.
+   * @protected @override
+   */
+  protected override getGeoviewLayerSchema(): string {
+    /** The GeoView layer schema associated to GeoJsonLayerConfig */
+    return CV_GEOVIEW_SCHEMA_PATH.GEOJSON;
+  }
+
+  /**
+   * The method used to implement the class factory model that returns the instance of the class based on the sublayer
+   * type needed.
+   *
+   * @param {TypeJsonObject} layerConfig The sublayer configuration.
+   * @param {TypeDisplayLanguage} language The initial language to use when interacting with the geoview layer.
+   * @param {AbstractGeoviewLayerConfig} geoviewConfig The GeoView instance that owns the sublayer.
+   * @param {EntryConfigBaseClass} parentNode The The parent node that owns this layer or undefined if it is the root layer.
+   *
+   * @returns {EntryConfigBaseClass} The sublayer instance or undefined if there is an error.
+   * @override
+   */
+  override createLeafNode(
+    layerConfig: TypeJsonObject,
+    language: TypeDisplayLanguage,
+    geoviewConfig: AbstractGeoviewLayerConfig,
+    parentNode?: EntryConfigBaseClass
+  ): EntryConfigBaseClass {
+    return new GeoJsonLayerEntryConfig(layerConfig, language, geoviewConfig, parentNode);
+  }
+
+  /**
+   * The method used to implement the class factory model that returns the instance of the class based on the group
+   * type needed.
+   *
+   * @param {TypeJsonObject} layerConfig The group node configuration.
+   * @param {TypeDisplayLanguage} language The initial language to use when interacting with the geoview layer.
+   * @param {AbstractGeoviewLayerConfig} geoviewConfig The GeoView instance that owns the sublayer.
+   * @param {EntryConfigBaseClass} parentNode The The parent node that owns this layer or undefined if it is the root layer.
+   *
+   * @returns {EntryConfigBaseClass} The sublayer instance or undefined if there is an error.
+   * @override
+   */
+  override createGroupNode(
+    layerConfig: TypeJsonObject,
+    language: TypeDisplayLanguage,
+    geoviewConfig: AbstractGeoviewLayerConfig,
+    parentNode?: EntryConfigBaseClass
+  ): EntryConfigBaseClass {
+    return new GeoJsonGroupLayerConfig(layerConfig, language, geoviewConfig, parentNode);
+  }
+
+  /**
+   * Get the service metadata from the metadataAccessPath and store it in the private property of the geoview layer.
+   * @override @async
+   */
+  override async fetchServiceMetadata(): Promise<void> {
+    try {
+      if (this.metadataAccessPath.toLowerCase().endsWith('.json') || this.metadataAccessPath.toLowerCase().endsWith('.geojson')) {
+        const layerMetadata = (await (await fetch(this.metadataAccessPath)).json()) as TypeJsonObject;
+        if (layerMetadata) this.setServiceMetadata(layerMetadata);
+        else throw new GeoviewLayerConfigError('The metadata object returned is undefined');
+      } else {
+        await this.createLayerTree();
+        return;
+      }
+
+      this.listOfLayerEntryConfig = this.processListOfLayerEntryConfig(this.listOfLayerEntryConfig);
+      await this.fetchListOfLayerMetadata();
+
+      await this.createLayerTree();
+    } catch (error) {
+      // In the event of a service metadata reading error, we report the geoview layer and all its sublayers as being in error.
+      this.setErrorDetectedFlag();
+      this.setErrorDetectedFlagForAllLayers(this.listOfLayerEntryConfig);
+      logger.logError(`Error detected while reading GeoJson metadata for geoview layer ${this.geoviewLayerId}.\n`, error);
+    }
+  }
+
+  /**
+   * Create a layer entry node for a specific layerId using the service metadata. The node returned can only be a
+   * layer leaf or a layer group.
+   *
+   * @param {string} layerId The layer id to use for the subLayer creation.
+   * @param {EntryConfigBaseClass | undefined} parentNode The layer's parent node.
+   *
+   * @returns {EntryConfigBaseClass} The subLayer created from the metadata.
+   * @protected @override
+   */
+  protected override createLayerEntryNode(layerId: string, parentNode: EntryConfigBaseClass | undefined): EntryConfigBaseClass {
+    // If we cannot find the layerId in the layer definitions, throw an error.
+    const layerFound = this.findLayerMetadataEntry(layerId);
+    if (!layerFound) {
+      throw new GeoviewLayerInvalidParameterError('LayerIdNotFound', [layerId?.toString()]);
+    }
+
+    if (layerEntryIsGroupLayer(layerFound)) return this.createGroupNode(layerFound, this.getLanguage(), this, parentNode);
+    return this.createLeafNode(layerFound, this.getLanguage(), this, parentNode)!;
+  }
+
+  /**
+   * Create the layer tree using the service metadata.
+   *
+   * @returns {TypeJsonObject[]} The layer tree created from the metadata.
+   * @protected @override
+   */
+  protected override createLayerTreeFromServiceMetadata(): EntryConfigBaseClass[] {
+    const layerTree = this.getServiceMetadata().listOfLayerEntryConfig;
+    if (layerEntryIsGroupLayer(layerTree)) return [this.createGroupNode(layerTree, this.getLanguage(), this)];
+    return [this.createLeafNode(layerTree, this.getLanguage(), this)!];
+  }
+  // #endregion OVERRIDE
+
+  // ==============
+  // #region PUBLIC
+  /** ****************************************************************************************************************************
+   * This method search recursively the layerId in the layer entry of the service metadata.
+   *
+   * @param {string} layerId The layer identifier that must exists on the server.
+   *
+   * @returns {TypeJsonObject | null} The found layer from the capabilities or null if not found.
+   */
+  findLayerMetadataEntry(layerId: string, listOfLayerEntryConfig = this.getServiceMetadata().listOfLayerEntryConfig as TypeJsonArray): TypeJsonObject | null {
+    return listOfLayerEntryConfig.reduce((layerFound, layerEntry) => {
+      if (layerFound) return layerFound;
+  
+      if (layerEntry.layerId === layerId) {
+        return layerEntry; 
+      }
+  
+      if (layerEntry.isLayerGroup || layerEntry.entryType === CV_CONST_SUB_LAYER_TYPES.GROUP) {
+        return this.findLayerMetadataEntry(layerId, layerEntry.listOfLayerEntryConfig as TypeJsonArray);
+      }
+  
+      return null;
+    }, null as TypeJsonObject | null);
+  }
+  
+  // #endregion PUBLIC
+  // #endregion METHODS
+  // #endregion CLASS HEADER
+}
