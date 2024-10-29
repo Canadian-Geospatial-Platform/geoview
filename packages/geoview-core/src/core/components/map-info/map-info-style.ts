da03280809a6f45f32cd3a4abda07d4b712a2ba4
Pascal Vautour <vautour.pascal@gmail.com>
2024-10-29 16:04:03 -0400

    fix footer bar.

--- packages/geoview-core/src/core/components/map-info/map-info-style.ts
+++ packages/geoview-core/src/core/components/map-info/map-info-style.ts
@@ -1,47 +1,5 @@
 // map-info.tsx
-import { Theme } from '@mui/material/styles';
-
-// ? I doubt we want to define an explicit type for style properties?
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const getSxClasses = (theme: Theme): any => ({
-  mapInfoContainer: {
-    flexGrow: 1,
-    zIndex: 200,
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    width: 'calc(100%)',
-    minHeight: '35px',
-    maxHeight: '35px',
-    backdropFilter: 'blur(5px)',
-    backgroundColor: theme.palette.geoViewColor.bgColor.dark[800],
-    color: `${theme.palette.geoViewColor.bgColor.light[800]} !important`,
-    pointerEvents: 'all',
-    gap: 0.5,
-    order: 3,
-    fill: `${theme.palette.geoViewColor.grey.dark[900]} !important`,
-    position: 'absolute',
-    bottom: 0,
-    left: '0px',
-
-    '&.interaction-static': {
-      backdropFilter: 'unset',
-      backgroundColor: 'unset',
-      color: `${theme.palette.geoViewColor.grey.dark[900]} !important`,
-      fill: `${theme.palette.geoViewColor.grey.dark[900]} !important`,
-      position: 'absolute',
-      width: 'calc(100% - 60px)',
-      bottom: 0,
-      left: '60px',
-
-      '& button svg': {
-        fill: `${theme.palette.geoViewColor.grey.dark[900]} !important`,
-      },
-      '& button span': {
-        color: `${theme.palette.geoViewColor.grey.dark[900]} !important`,
-      },
-    },
-  },
+export const getSxClasses = () => ({
   mouseScaleControlsContainer: {
     display: 'flex',
     flexDirection: 'row',
@@ -55,33 +13,11 @@ export const getSxClasses = (theme: Theme): any => ({
   rotationControlsContainer: {
     display: 'flex',
     flexDirection: 'column',
-    marginLeft: '20px',
     alignItems: 'flex-end',
   },
 
   // map-info-expand-button.tsx
   expandButton: {
     display: { xs: 'none', sm: 'none', md: 'flex', lg: 'flex', xl: 'flex' },
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: theme.palette.geoViewColor.bgColor.light[800],
-    height: '30px',
-    width: '30px',
-    marginLeft: '5px',
-  },
-
-  // map-info-rotation-button.tsx
-  rotationButton: {
-    rotationButton: {
-      height: 25,
-      width: 25,
-      marginRight: 5,
-    },
-    rotationIcon: {
-      width: '1.5em',
-      height: '1.5em',
-      fontSize: theme.palette.geoViewFontSize.default,
-      color: theme.palette.geoViewColor.bgColor.light[800],
-    },
   },
 });
--- packages/geoview-core/src/core/components/map-info/map-info.tsx
+++ packages/geoview-core/src/core/components/map-info/map-info.tsx
@@ -1,7 +1,6 @@
-import { MutableRefObject, useRef } from 'react';
 import { useTheme } from '@mui/material/styles';
 
-import { Box, Grid } from '@/ui';
+import { Box } from '@/ui';
 import { Attribution } from '@/core/components/attribution/attribution';
 import { MousePosition } from '@/core/components/mouse-position/mouse-position';
 import { Scale } from '@/core/components/scale/scale';
@@ -9,11 +8,10 @@ import { Scale } from '@/core/components/scale/scale';
 import { MapInfoExpandButton } from './map-info-expand-button';
 import { MapInfoRotationButton } from './map-info-rotation-button';
 import { MapInfoFixNorthSwitch } from './map-info-fixnorth-switch';
-import { getSxClasses } from './map-info-style';
+// import { getSxClasses } from './map-info-style';
 import { useMapInteraction } from '@/core/stores/store-interface-and-intial-values/map-state';
 import { useUIMapInfoExpanded } from '@/core/stores/store-interface-and-intial-values/ui-state';
 import { logger } from '@/core/utils/logger';
-import { useGeoViewMapId } from '@/core/stores/geoview-store';
 
 /**
  * Create a map information element that contains attribtuion, mouse position and scale
@@ -24,13 +22,7 @@ export function MapInfo(): JSX.Element {
   // Log
   logger.logTraceRender('components/map-info/map-info');
 
-  const mapId = useGeoViewMapId();
-
   const theme = useTheme();
-  const sxClasses = getSxClasses(theme);
-
-  // internal state
-  const mapInfoRef = useRef<HTMLDivElement>();
 
   // get store values
   const expanded = useUIMapInfoExpanded();
@@ -41,53 +33,31 @@ export function MapInfo(): JSX.Element {
 
   return (
     <Box
-      id={`${mapId}-mapInfo`}
-      className={`interaction-${interaction}`}
-      sx={sxClasses.mapInfoContainer}
-      ref={mapInfoRef as MutableRefObject<HTMLDivElement>}
+      sx={{
+        display: 'flex',
+        height: expanded ? '10rem' : '3rem',
+        alignItems: 'center',
+        transition: 'width 0.5s, height 0.5s',
+        background: theme.palette.geoViewColor.bgColor.dark[800],
+        color: theme.palette.geoViewColor.bgColor.light[800],
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        px: '1rem',
+      }}
     >
-      {interaction === 'dynamic' && <MapInfoExpandButton />}
-      <Grid container justifyContent="space-between" size={12}>
-        {interaction === 'dynamic' && (
-          <Grid size={{ md: 1 }}>
-            <Attribution />
-          </Grid>
-        )}
-
-        <Grid container spacing={2} size={{ md: interaction === 'dynamic' ? 11 : 12 }}>
-          <Grid
-            container
-            sx={{
-              justifyContent: 'flex-end',
-              flexWrap: 'nowrap',
-              width: '100%',
-            }}
-          >
-            <Grid size={{ md: interaction === 'dynamic' ? 11 : 12 }}>
-              <Box id="mouseAndScaleControls" sx={sxClasses.mouseScaleControlsContainer}>
-                {interaction === 'dynamic' && <MousePosition />}
-                <Scale />
-              </Box>
-            </Grid>
-            {interaction === 'dynamic' && (
-              <Grid size={{ md: 2 }}>
-                <Box
-                  sx={{
-                    ...sxClasses.rotationControlsContainer,
-                    marginTop: !expanded ? '5px' : '10px',
-                    [theme.breakpoints.down('md')]: {
-                      marginTop: expanded ? '10px' : 'none',
-                    },
-                  }}
-                >
-                  <MapInfoRotationButton />
-                  <MapInfoFixNorthSwitch />
-                </Box>
-              </Grid>
-            )}
-          </Grid>
-        </Grid>
-      </Grid>
+      {interaction === 'dynamic' && (
+        <>
+          <MapInfoExpandButton />
+          <div style={{ flexGrow: 1 }} />
+          <MousePosition />
+          <MapInfoRotationButton />
+          <MapInfoFixNorthSwitch />
+        </>
+      )}
+      <Scale />
+      <Attribution />
     </Box>
   );
 }
