279c97826cd8bcf29440c73661e9d13233c202ee
jolevesq <johann.levesque@nrcan-rncan.gc.ca>
2024-12-10 11:27:17 -0500

    Rebase

--- packages/geoview-core/src/geo/layer/gv-layers/raster/gv-wms.ts
+++ packages/geoview-core/src/geo/layer/gv-layers/raster/gv-wms.ts
@@ -119,17 +119,16 @@ export class GVWMS extends AbstractGVRaster {
       const layerConfig = this.getLayerConfig();
 
       // Check if bounds are properly set
-      // TODO: We always do the check if not bounds are not set properly from layer creation process
-      //  if (!layerConfig.initialSettings!.bounds) {
-      const newBounds = this.getBounds(this.getLayerPath());
-      if (newBounds)
-        layerConfig.initialSettings!.bounds = Projection.transformExtentFromProj(
-          newBounds,
-          this.getMapViewer().getView().getProjection(),
-          Projection.PROJECTION_NAMES.LNGLAT
-        );
-      //    else return [];
-      //  }
+      if (!layerConfig.initialSettings!.bounds) {
+        const newBounds = this.getBounds();
+        if (newBounds)
+          layerConfig.initialSettings!.bounds = Projection.transformExtentFromProj(
+            newBounds,
+            this.getMapViewer().getView().getProjection(),
+            Projection.PROJECTION_NAMES.LNGLAT
+          );
+        else return [];
+      }
 
       const clickCoordinate = this.getMapViewer().convertCoordinateLngLatToMapProj(lnglat);
       if (
