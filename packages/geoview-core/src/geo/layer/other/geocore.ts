d4059797e5c2031f049ae06a6cb73579c57107bf
MatthewMuehlhauserNRCan <Matthew.Muehlhauser@NRCan-RNCan.gc.ca>
2024-12-11 10:02:28 -0500

    rename/match config parameter

--- packages/geoview-core/src/geo/layer/other/geocore.ts
+++ packages/geoview-core/src/geo/layer/other/geocore.ts
@@ -33,10 +33,10 @@ export class GeoCore {
   /**
    * Gets GeoView layer configurations list from the UUIDs of the list of layer entry configurations.
    * @param {string} uuid the UUID of the layer
-   * @param {GeoCoreLayerConfig} geoCoreLayerConfig the layer configuration
+   * @param {GeoCoreLayerConfig} layerConfig the layer configuration
    * @returns {Promise<TypeGeoviewLayerConfig[]>} list of layer configurations to add to the map
    */
-  async createLayersFromUUID(uuid: string, layerEntryConfig?: GeoCoreLayerConfig): Promise<TypeGeoviewLayerConfig[]> {
+  async createLayersFromUUID(uuid: string, layerConfig?: GeoCoreLayerConfig): Promise<TypeGeoviewLayerConfig[]> {
     // Get the map config
     const mapConfig = MapEventProcessor.getGeoViewMapConfig(this.#mapId);
 
@@ -51,15 +51,15 @@ export class GeoCore {
       ConfigValidation.validateListOfGeoviewLayerConfig(this.#displayLanguage, response.layers);
 
       // Set the initialSettings parameter if present
-      if (layerEntryConfig?.initialSettings) {
+      if (layerConfig?.initialSettings) {
         for (let i = 0; i < response.layers.length; i++) {
-          response.layers[i].initialSettings = { ...response.layers[i].initialSettings, ...layerEntryConfig.initialSettings };
-          // Need to set the initial settings of the LayerEntryConfigs. Only applies to top layers.
+          response.layers[i].initialSettings = { ...response.layers[i].initialSettings, ...layerConfig.initialSettings };
+          // Need to set the initial settings of the LayerConfigs. Only applies to top layers.
           // Visibility is passed down to children, but other values may not.
           for (let j = 0; j < response.layers[i].listOfLayerEntryConfig.length; j++) {
             response.layers[i].listOfLayerEntryConfig[j].initialSettings = {
               ...response.layers[i].listOfLayerEntryConfig[j].initialSettings,
-              ...layerEntryConfig.initialSettings,
+              ...layerConfig.initialSettings,
             };
           }
         }
