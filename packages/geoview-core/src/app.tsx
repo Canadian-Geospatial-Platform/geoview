c208e7f704ee2f44eda5e16f86689129f29f33b4
Damon Ulmi <63123585+DamonU2@users.noreply.github.com>
2025-01-10 10:08:02 -0800

    fix(apostrophe): fixes issue with apostrophe breaking config

--- packages/geoview-core/src/app.tsx
+++ packages/geoview-core/src/app.tsx
@@ -189,7 +189,7 @@ export async function initMapDivFromFunctionCall(mapDiv: HTMLElement, mapConfig:
     // Create a data-config attribute and set config value on the div
     const att = document.createAttribute(url ? 'data-config-url' : 'data-config');
     // Clean apostrophes in the config
-    att.value = JSON.stringify(mapConfig).replace(/'/g, "\\'");
+    att.value = mapConfig.replace(/'/g, "\\'");
     mapDiv.setAttributeNode(att);
 
     // Set the geoview-map class on the div so that this class name is standard for all maps (either created via init or via func call)
