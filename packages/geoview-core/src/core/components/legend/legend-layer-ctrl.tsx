b4cce4abd3463ed2544589874a4c13f4520c81c3
AlexNRCan <alexandre.roy@nrcan-rncan.gc.ca>
2025-02-14 12:33:34 -0500

    Cleared a warning

--- packages/geoview-core/src/core/components/legend/legend-layer-ctrl.tsx
+++ packages/geoview-core/src/core/components/legend/legend-layer-ctrl.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from '@mui/material';
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
   Box,
