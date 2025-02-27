cca466ac99e9fd2062eb2999a9ca3f51ecf1cdce
Hans A <41074804+HansAlainNyaba@users.noreply.github.com>
2025-02-27 12:32:10 -0500

    Fix - export disclaimer message fixing last commit after rebase

--- packages/geoview-core/src/core/components/export/export-modal.tsx
+++ packages/geoview-core/src/core/components/export/export-modal.tsx
@@ -23,7 +23,6 @@ export default function ExportModal(): JSX.Element {
   const { t } = useTranslation();
   const mapId = useGeoViewMapId();
   const fileExportDefaultPrefixName = t('exportModal.fileExportDefaultPrefixName');
-  const disclaimerTitle = t('mapctrl.disclaimer.title');
   const disclaimerMessage = t('mapctrl.disclaimer.message');
   const mapElement = useAppGeoviewHTMLElement();
   const mapViewport = mapElement.getElementsByClassName('ol-viewport')[0];
@@ -213,7 +212,7 @@ export default function ExportModal(): JSX.Element {
               </Box>
             ))}
           </Box>
-          <Box textAlign="center" key={disclaimerMessage} component="p" sx={{ margin: 0 }}>
+          <Box textAlign="center" key={disclaimerMessage} component="p" sx={{ margin: 0, marginBottom: '20px' }}>
             {disclaimerMessage}
           </Box>
           <Box textAlign="center" sx={{ marginBottom: '1rem' }}>
