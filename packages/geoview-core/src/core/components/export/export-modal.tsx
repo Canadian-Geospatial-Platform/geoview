6a2a153dc26f2d6af623865b710dd78969be420b
Hans A <41074804+HansAlainNyaba@users.noreply.github.com>
2025-03-13 18:29:06 -0400

    fix(export): removed mini map from export Closes #2704

--- packages/geoview-core/src/core/components/export/export-modal.tsx
+++ packages/geoview-core/src/core/components/export/export-modal.tsx
@@ -117,6 +117,8 @@ export default function ExportModal(): JSX.Element {
     setLegendLayers(layersList);
     const overviewMap = mapElement.getElementsByClassName('ol-overviewmap')[0] as HTMLDivElement;
 
+    const overviewMap = mapElement.getElementsByClassName('ol-overviewmap')[0] as HTMLDivElement;
+
     let timer: NodeJS.Timeout;
     if (activeModalId === 'export' && mapImageRef.current && dialogRef.current) {
       const mapImage = mapImageRef.current;
