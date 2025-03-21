8f450cc269bb8d6a18c9f722c8f6fa69d71ab497
Farah <ahmedyacin.farah@nrcan-rncan.gc.ca>
2025-03-21 15:00:45 -0400

    reorder of custom-legend  loading packages

--- packages/geoview-core/src/api/config/types/config-constants.ts
+++ packages/geoview-core/src/api/config/types/config-constants.ts
@@ -229,7 +229,16 @@ export const CV_DEFAULT_LAYER_INITIAL_SETTINGS = {
 /**
  * Definition of the default order of the tabs inside appbar
  */
-export const CV_DEFAULT_APPBAR_TABS_ORDER = ['geolocator', 'aoi-panel', 'legend', 'layers', 'details', 'data-table', 'guide'];
+export const CV_DEFAULT_APPBAR_TABS_ORDER = [
+  'geolocator',
+  'aoi-panel',
+  'legend',
+  'custom-legend',
+  'layers',
+  'details',
+  'data-table',
+  'guide',
+];
 
 export const CV_DEFAULT_APPBAR_CORE = {
   GEOLOCATOR: 'geolocator',
