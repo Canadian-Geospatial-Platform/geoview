1c0930d761367f735f13248eedcaa4af645269ad
kavery <kavery@compusult.net>
2025-05-08 10:02:28 -0230

    fix(3d): fixed an error where the bounds were coming back with infinity.

--- packages/geoview-core/src/core/components/cesium/cesium-map.tsx
+++ packages/geoview-core/src/core/components/cesium/cesium-map.tsx
@@ -237,6 +237,9 @@ function setOLMapExtent(oViewer: MapViewer, cViewer: Viewer): void {
   const east = parseFloat(CesiumMath.toDegrees(rect!.east).toFixed(10));
   const north = parseFloat(CesiumMath.toDegrees(rect!.north).toFixed(10));
   const projdExtent = transformExtent([west, south, east, north], 'EPSG:4326', oViewer.getProjection().getCode());
+  if (projdExtent.some((element) => !Number.isFinite(element))) {
+    return;
+  }
   try {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     MapEventProcessor.zoomToExtent(oViewer.mapId, projdExtent, { padding: [1, 1, 1, 1], maxZoom: 30, duration: 1 }).catch((_e) => {});
