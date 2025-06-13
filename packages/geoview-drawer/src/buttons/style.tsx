00f833ce927b86d0d11c06657fd9558872845507
MatthewMuehlhauserNRCan <Matthew.Muehlhauser@NRCan-RNCan.gc.ca>
2025-06-13 15:07:14 -0400

    Style Button and small fixes

--- packages/geoview-drawer/src/buttons/style.tsx
+++ packages/geoview-drawer/src/buttons/style.tsx
@@ -1,22 +1,33 @@
-import React, { createElement, ReactNode, useCallback } from 'react';
+import { TypeWindow } from 'geoview-core';
+import { ChangeEvent } from 'react';
 import { useDrawerStyle, useDrawerActions } from 'geoview-core/src/core/stores/store-interface-and-intial-values/drawer-state';
 import { useAppDisplayLanguage } from 'geoview-core/src/core/stores/store-interface-and-intial-values/app-state';
 import { getLocalizedMessage } from 'geoview-core/src/core/utils/utilities';
 import { logger } from 'geoview-core/src/core/utils/logger';
 
-import NavbarPanelButton from 'geoview-core/src/core/components/nav-bar/nav-bar-panel-button';
-import { PaletteIcon } from 'geoview-core/src/ui/icons';
-import { TypePanelProps } from 'geoview-core/src/ui/panel/panel-types';
-import { IconButtonPropsExtend } from 'geoview-core/src/ui/icon-button/icon-button';
-import { Box, Typography } from 'geoview-core/src/ui';
+export function StyleButton(): JSX.Element {
+  const { PaletteIcon, IconButton } = (window as TypeWindow).cgpv.ui.elements;
+  return (
+    <IconButton id="clear">
+      <PaletteIcon />
+    </IconButton>
+  );
+}
 
 /**
  * Create a style button with a panel to customize drawing styles
  * @returns {JSX.Element} the created style button with panel
  */
-export default function Style(): JSX.Element {
+export function StylePanel(): JSX.Element {
   // Log
   logger.logTraceRender('geoview-drawer/src/buttons/style');
+
+  const { cgpv } = window as TypeWindow;
+  const { ui, react } = cgpv;
+  const { useCallback } = react;
+
+  // Components
+  const { List, ListItem, Typography } = ui.elements;
 
   // Get store values
   const style = useDrawerStyle();
@@ -26,21 +37,21 @@ export default function Style(): JSX.Element {
   const { setFillColor, setStrokeColor, setStrokeWidth } = useDrawerActions();
 
   const handleFillColorChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>): void => {
+    (event: ChangeEvent<HTMLInputElement>): void => {
       setFillColor(event.target.value);
     },
     [setFillColor]
   );
 
   const handleStrokeColorChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>): void => {
+    (event: ChangeEvent<HTMLInputElement>): void => {
       setStrokeColor(event.target.value);
     },
     [setStrokeColor]
   );
 
   const handleStrokeWidthChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>): void => {
+    (event: ChangeEvent<HTMLInputElement>): void => {
       const width = parseInt(event.target.value as string, 10);
       if (Number.isNaN(width)) return;
       setStrokeWidth(width);
@@ -52,42 +63,22 @@ export default function Style(): JSX.Element {
    * Render style controls in navbar panel
    * @returns ReactNode
    */
-  const renderStyleControls = (): ReactNode => {
-    return (
-      <Box sx={{ p: 2 }}>
-        <Box sx={{ mb: 2 }}>
-          <Typography variant="subtitle2">{getLocalizedMessage(displayLanguage, 'drawer.fillColour')}</Typography>
-          <input type="color" value={style.fillColor} onChange={(e) => handleFillColorChange(e)} />
-        </Box>
+  return (
+    <List sx={{ p: 2 }}>
+      <ListItem sx={{ mb: 2 }}>
+        <Typography variant="subtitle2">{getLocalizedMessage(displayLanguage, 'drawer.fillColour')}</Typography>
+        <input type="color" value={style.fillColor} onChange={(e) => handleFillColorChange(e)} />
+      </ListItem>
 
-        <Box sx={{ mb: 2 }}>
-          <Typography variant="subtitle2">{getLocalizedMessage(displayLanguage, 'drawer.strokeColour')}</Typography>
-          <input type="color" value={style.strokeColor} onChange={(e) => handleStrokeColorChange(e)} />
-        </Box>
+      <ListItem sx={{ mb: 2 }}>
+        <Typography variant="subtitle2">{getLocalizedMessage(displayLanguage, 'drawer.strokeColour')}</Typography>
+        <input type="color" value={style.strokeColor} onChange={(e) => handleStrokeColorChange(e)} />
+      </ListItem>
 
-        <Box>
-          <Typography variant="subtitle2">{getLocalizedMessage(displayLanguage, 'drawer.strokeWidth')}</Typography>
-          <input type="number" value={style.strokeWidth} min="0" max="10" onChange={(e) => handleStrokeWidthChange(e)} />
-        </Box>
-      </Box>
-    );
-  };
-
-  // Set up props for nav bar panel button
-  const button: IconButtonPropsExtend = {
-    // id: 'drawer-style',
-    tooltip: 'drawer.style',
-    children: createElement(PaletteIcon),
-    tooltipPlacement: 'right',
-    // visible: true,
-  };
-
-  const panel: TypePanelProps = {
-    title: 'drawer.style',
-    icon: createElement(PaletteIcon),
-    content: renderStyleControls(),
-    width: 300,
-  };
-
-  return <NavbarPanelButton buttonPanel={{ buttonPanelId: 'drawerStyle', button, panel }} />;
+      <ListItem>
+        <Typography variant="subtitle2">{getLocalizedMessage(displayLanguage, 'drawer.strokeWidth')}</Typography>
+        <input type="number" value={style.strokeWidth} min="0" max="10" onChange={(e) => handleStrokeWidthChange(e)} />
+      </ListItem>
+    </List>
+  );
 }
