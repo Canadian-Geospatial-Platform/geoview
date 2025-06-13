ff4dcef89215c648da5ef0625c57ce0d8d77253d
MatthewMuehlhauserNRCan <Matthew.Muehlhauser@NRCan-RNCan.gc.ca>
2025-06-13 12:03:59 -0400

    Text only working version

--- packages/geoview-drawer/src/buttons/geometry-picker.tsx
+++ packages/geoview-drawer/src/buttons/geometry-picker.tsx
@@ -1,7 +1,7 @@
 import { TypeWindow } from 'geoview-core';
 import { useDrawerGeomType } from 'geoview-core/src/core/stores/store-interface-and-intial-values/drawer-state';
 
-import { IconButton } from 'geoview-core/src/ui/icon-button/icon-button';
+import { IconButton, Button } from 'geoview-core/src/ui';
 // import { logger } from 'geoview-core/src/core/utils/logger';
 
 /**
@@ -11,29 +11,29 @@ import { IconButton } from 'geoview-core/src/ui/icon-button/icon-button';
  */
 export default function GeometryPicker(): JSX.Element {
   const { cgpv } = window as TypeWindow;
-  const { RadioButtonUncheckedIcon, LinearScaleIcon, PolygonIcon, CircleIcon } = cgpv.ui.elements;
+  // const { RadioButtonUncheckedIcon, LinearScaleIcon, PolygonIcon, CircleIcon } = cgpv.ui.elements;
 
   // Get store values
   const geomType = useDrawerGeomType();
 
   // Create the icon element based on geomType
-  let iconElement;
-  switch (geomType) {
-    case 'Point':
-      iconElement = <RadioButtonUncheckedIcon />;
-      break;
-    case 'LineString':
-      iconElement = <LinearScaleIcon />;
-      break;
-    case 'Polygon':
-      iconElement = <PolygonIcon />;
-      break;
-    case 'Circle':
-      iconElement = <CircleIcon />;
-      break;
-    default:
-      iconElement = <RadioButtonUncheckedIcon />;
-  }
+  // let iconElement;
+  // switch (geomType) {
+  //   case 'Point':
+  //     iconElement = <RadioButtonUncheckedIcon />;
+  //     break;
+  //   case 'LineString':
+  //     iconElement = <LinearScaleIcon />;
+  //     break;
+  //   case 'Polygon':
+  //     iconElement = <PolygonIcon />;
+  //     break;
+  //   case 'Circle':
+  //     iconElement = <CircleIcon />;
+  //     break;
+  //   default:
+  //     iconElement = <RadioButtonUncheckedIcon />;
+  // }
 
-  return <IconButton id="draw">{iconElement}</IconButton>;
+  return <Button id="draw">{geomType}</Button>;
 }
