becbb3cb2bbf2d71c64f3b1e531f45867cc2d8d4
Johann Levesque <johann.levesque@canada.ca>
2025-06-19 12:15:58 -0400

    One line from Damon and fix the more info time max value

--- packages/geoview-core/src/core/components/layers/right-panel/layer-details.tsx
+++ packages/geoview-core/src/core/components/layers/right-panel/layer-details.tsx
@@ -412,7 +412,7 @@ export function LayerDetails(props: LayerDetailsProps): JSX.Element {
           {layerNativeProjection && <Box>{`${t('layers.layerServiceProjection')}${layerNativeProjection}`}</Box>}
           {layerFilter && <Box>{`${t('layers.layerDefaultFilter')}${layerFilter}`}</Box>}
           {layerTemporalDimension && (
-            <Box>{`${t('layers.layerTemporalDimension')}${t('layers.layerTemporalDimensionField')} - ${layerTemporalDimension.field} -, min - ${layerTemporalDimension.range.range[0]} / max - ${layerTemporalDimension.range.range[1]}`}</Box>
+            <Box>{`${t('layers.layerTemporalDimension')}${t('layers.layerTemporalDimensionField')} - ${layerTemporalDimension.field} -, min - ${layerTemporalDimension.range.range[0]} / max - ${layerTemporalDimension.range.range[layerTemporalDimension.range.range.length - 1]}`}</Box>
           )}
           {resources !== '' && (
             <Box className="info-container">
