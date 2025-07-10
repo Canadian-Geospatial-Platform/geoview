6996d708c934398f6fe0edad832fdaaf71456fbf
MatthewMuehlhauserNRCan <Matthew.Muehlhauser@NRCan-RNCan.gc.ca>
2025-07-10 15:20:55 -0400

    small fix

--- packages/geoview-core/src/geo/interaction/transform/index.ts
+++ packages/geoview-core/src/geo/interaction/transform/index.ts
@@ -2,3 +2,4 @@ export * from './transform';
 export * from './transform-base';
 export * from './events/transform-event';
 export * from './events/delete-event';
+export * from './events/select-event';
