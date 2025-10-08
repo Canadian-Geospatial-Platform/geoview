ee80042f389416b0fb858803e7229ded1c0f0888
MatthewMuehlhauserNRCan <Matthew.Muehlhauser@NRCan-RNCan.gc.ca>
2025-10-08 14:46:12 -0400

    Comment fixes

---
+++ packages/geoview-core/src/core/components/export/export-modal-style.ts
@@ -0,0 +1,26 @@
+import { Theme } from '@mui/material/styles';
+import { SxStyles } from '@/ui/style/types';
+
+export const getSxClasses = (theme: Theme): SxStyles => ({
+  buttonOutlined: {
+    fontSize: theme.palette.geoViewFontSize.sm,
+    padding: '0.7rem 1rem',
+    height: '47px',
+    borderColor: theme.palette.geoViewColor.primary.main,
+    color: theme.palette.geoViewColor.primary.main,
+    '&:hover': {
+      borderColor: theme.palette.geoViewColor.primary.dark[200],
+      backgroundColor: theme.palette.geoViewColor.primary.light[100],
+    },
+  },
+
+  buttonContained: {
+    fontSize: theme.palette.geoViewFontSize.sm,
+    padding: '0.7rem 1rem',
+    backgroundColor: theme.palette.geoViewColor.primary.main,
+    height: '47px',
+    '&:hover': {
+      backgroundColor: theme.palette.geoViewColor.primary.dark[200],
+    },
+  },
+});
