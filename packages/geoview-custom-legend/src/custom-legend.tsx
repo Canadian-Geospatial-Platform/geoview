3c9f9d33f16c669a22776dc786ebf0dd99f7bbfb
Hans A <41074804+HansAlainNyaba@users.noreply.github.com>
2025-11-13 07:35:26 -0500

    Custom-Legend-Schema development Closes #2363

--- packages/geoview-custom-legend/src/custom-legend.tsx
+++ packages/geoview-custom-legend/src/custom-legend.tsx
@@ -1,5 +1,5 @@
 import type { TypeWindow } from 'geoview-core/core/types/global-types';
-import { getSxClasses } from './custom-legend-style';
+import { getSxClassesMain, getSxClasses } from './custom-legend-style';
 
 interface CustomLegendPanelProps {
   config: TypeLegendProps;
@@ -24,40 +24,38 @@ export function CustomLegendPanel(props: CustomLegendPanelProps): JSX.Element {
   const { legendList } = config;
 
   const { cgpv } = window as TypeWindow;
-  const { ui } = cgpv;
+  const { ui, reactUtilities } = cgpv;
+  const { useState } = reactUtilities.react;
   const { Card, Box } = ui.elements;
 
   const theme = ui.useTheme();
   const sxClasses = getSxClasses(theme);
+  const legendSxMain = getSxClassesMain();
+
+  const [activeLegendList] = useState<LegendListItems>(legendList);
 
   return (
-    <Box sx={sxClasses.legendCard}>
-      {legendList.map((legendItem: LegendItem, index) => {
-        return (
+    <Box sx={{ background: theme.palette.geoViewColor.bgColor.main, ...legendSxMain.container }}>
+      {activeLegendList.map((legendItem: LegendItem) => (
+        <Box key={`${legendItem.legendTitle}-${legendItem.symbolUrl}`} sx={sxClasses.legendLayerListItem}>
           <Card
             tabIndex={0}
             className="legendCardItem"
-            // eslint-disable-next-line react/no-array-index-key
-            key={index}
             title={legendItem.legendTitle}
             contentCard={
-              // eslint-disable-next-line react/jsx-no-useless-fragment
-              <>
-                {typeof legendItem.symbolUrl === 'string' && (
-                  <div className="legend-item-container">
-                    {/* eslint-disable-next-line react/no-array-index-key */}
-                    <Box component="img" key={index} src={legendItem.symbolUrl} alt="" className="legendSymbol" />
-                    <div className="legend-text">
-                      <span className="legend-title">{legendItem.legendTitle}</span>
-                      {legendItem.description && <span className="legend-description">{legendItem.description}</span>}
-                    </div>
+              typeof legendItem.symbolUrl === 'string' ? (
+                <div className="legend-item-container">
+                  <Box component="img" src={legendItem.symbolUrl} alt="" className="legendSymbol" />
+                  <div className="legend-text">
+                    <span className="legend-title">{legendItem.legendTitle}</span>
+                    {legendItem.description && <span className="legend-description">{legendItem.description}</span>}
                   </div>
-                )}
-              </>
+                </div>
+              ) : null
             }
           />
-        );
-      })}
+        </Box>
+      ))}
     </Box>
   );
 }
