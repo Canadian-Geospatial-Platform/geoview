7bb0ee9e1c411b085a94fa141ae3cc9e3bf38153
Alex-NRCan <alexandre.roy@nrcan-rncan.gc.ca>
2025-12-19 18:44:55 -0500

    Updated the tests for the fix of the initialSettings pattern

--- packages/geoview-test-suite/src/tests/testers/layer-tester.ts
+++ packages/geoview-test-suite/src/tests/testers/layer-tester.ts
@@ -7,6 +7,7 @@ import type { TypeGeoviewLayerConfig } from 'geoview-core/api/types/layer-schema
 import type { AbstractGVLayer } from 'geoview-core/geo/layer/gv-layers/abstract-gv-layer';
 import { EsriDynamic } from 'geoview-core/geo/layer/geoview-layers/raster/esri-dynamic';
 import { generateId } from 'geoview-core/core/utils/utilities';
+import { AbstractBaseLayerEntryConfig } from 'geoview-core/api/config/validation-classes/abstract-base-layer-entry-config';
 import { LegendEventProcessor } from 'geoview-core/api/event-processors/event-processor-children/legend-event-processor';
 import { EsriFeature } from 'geoview-core/geo/layer/geoview-layers/vector/esri-feature';
 import { EsriImage } from 'geoview-core/geo/layer/geoview-layers/raster/esri-image';
@@ -1299,7 +1300,7 @@ export class LayerTester extends GVAbstractTester {
 
     // Expected config
     const expectedResults = {
-      groupHighlight: false,
+      groupHighlight: undefined,
       groupRemove: false,
       childHighlight: true,
     };
@@ -1317,20 +1318,23 @@ export class LayerTester extends GVAbstractTester {
       (test, result) => {
         const layer = result?.map?.listOfGeoviewLayerConfig.find(
           (geoviewLayer) => geoviewLayer.geoviewLayerId === config.geoviewLayerId
-          // GV: Casting as any to avoid TS error related to incompatible types between AbstractGeoViewLayerConfig and TypeGeoviewLayerConfig
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        ) as any;
+        ) as TypeGeoviewLayerConfig;
 
         // Perform assertions
+
         test.addStep('Verifying group layer highlight control...');
         Test.assertIsEqual(layer?.initialSettings?.controls?.highlight, expectedResults.groupHighlight);
 
         test.addStep('Verifying group layer remove control...');
-        Test.assertIsEqual(layer?.listOfLayerEntryConfig?.[0]?.initialSettings?.controls?.remove, expectedResults.groupRemove);
+        Test.assertIsEqual(
+          AbstractBaseLayerEntryConfig.getClassOrTypeInitialSettings(layer?.listOfLayerEntryConfig?.[0])?.controls?.remove,
+          expectedResults.groupRemove
+        );
 
         test.addStep('Verifying child layer highlight control...');
         Test.assertIsEqual(
-          layer?.listOfLayerEntryConfig?.[0]?.listOfLayerEntryConfig?.[0]?.initialSettings?.controls?.highlight,
+          AbstractBaseLayerEntryConfig.getClassOrTypeInitialSettings(layer?.listOfLayerEntryConfig?.[0]?.listOfLayerEntryConfig?.[0])
+            ?.controls?.highlight,
           expectedResults.childHighlight
         );
       },
