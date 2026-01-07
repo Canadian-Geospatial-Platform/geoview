d5afb3c1e290d4c4c420077504d5ddc3bfe9e389
MatthewMuehlhauserNRCan <Matthew.Muehlhauser@NRCan-RNCan.gc.ca>
2026-01-07 09:42:39 -0500

    Add decluttering TODO

--- packages/geoview-core/src/geo/layer/gv-layers/vector/abstract-gv-vector.ts
+++ packages/geoview-core/src/geo/layer/gv-layers/vector/abstract-gv-vector.ts
@@ -71,6 +71,11 @@ export abstract class AbstractGVVector extends AbstractGVLayer {
         // Return the style
         return style;
       },
+      // TODO: For layers with text, in order for declutterMode options to work, declutter at the layer level must be true
+      // TO.DOCONT: If true though, this will cause the features themselves to be decluttered, which we don't want
+      // TO.DOCONT: Instead, the best solution would be to create a second text only layer that uses the same source.
+      // TO.DOCONT: Could both layers be accessed by the same GeoView Layer? So the only the text layer or both layer's visibility can be toggled?
+      // declutter: true,
     };
 
     // Init the layer options with initial settings
