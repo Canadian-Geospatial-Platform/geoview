9065afdc36230ee5f82a40488a4d31eac115842e
Johann Levesque <johann.levesque@nrcan-rncan.gc.ca>
2026-05-05 13:33:09 -0400

    Add swiper package test

--- packages/geoview-test-suite/src/index.tsx
+++ packages/geoview-test-suite/src/index.tsx
@@ -15,6 +15,7 @@ import { GVTestSuiteMapConfig } from './tests/suites/suite-map-config';
 import { GVTestSuiteUI } from './tests/suites/suite-ui';
 import { GVTestSuiteDetails } from './tests/suites/suite-details';
 import { GVTestSuiteUtilities } from './tests/suites/suite-utilities';
+import { GVTestSuiteSwiper } from './tests/suites/suite-swiper';
 
 /**
  * Create a class for the plugin instance.
@@ -111,6 +112,9 @@ class TestSuitePlugin extends AbstractPlugin {
       } else if (suite === 'suite-utilities') {
         // Instanciate the GeoView Test Suite
         this.addTestSuite(new GVTestSuiteUtilities(window.cgpv.api, this.mapViewer, this.controllerRegistry));
+      } else if (suite === 'suite-swiper') {
+        // Instanciate the GeoView Test Suite
+        this.addTestSuite(new GVTestSuiteSwiper(window.cgpv.api, this.mapViewer, this.controllerRegistry));
       } else {
         // Throw
         throw new TestSuiteInitializationError(suite, this.mapViewer.mapId);
