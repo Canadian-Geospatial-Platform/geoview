c03bad4bed838a5f86fb7f06b84385611f958095
Alex-NRCan <alexandre.roy@nrcan-rncan.gc.ca>
2026-06-15 12:32:54 -0400

    Now showing a message on successful reload call and also improved a bit the tracking of the loadedOnce property Fixed the outlier-metadata.html template config

--- packages/geoview-core/src/core/controllers/layer-creator-controller.ts
+++ packages/geoview-core/src/core/controllers/layer-creator-controller.ts
@@ -475,6 +475,14 @@ export class LayerCreatorController extends AbstractMapViewerController {
 
               // Re-add on the map
               this.getMapViewer().map.addLayer(layer.getOLLayer());
+
+              // GV Cheat by pretending the layer was loaded once immediately
+              // GV Otherwise on next "reload layer" call it'll think it never was loaded ever (though it was at least once)
+              layer.loadedOnce = true;
+
+              // Show success of the layer reload
+              // GV As far as we're concerned, the reload worked, if the layer fails again, it'll reshow the fail error
+              this.getMapViewer().notifications.showSuccess('layers.layerReloaded', { layerName: layer.getLayerName() });
             } else {
               // Nonreloadable layer
               this.getMapViewer().notifications.showError('layers.errorNonreloadableLayer');
