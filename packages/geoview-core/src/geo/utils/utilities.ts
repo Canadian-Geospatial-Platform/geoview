e240aed73c586463ceae74dce105a9ef5b891a9c
Alex-NRCan <alexandre.roy@nrcan-rncan.gc.ca>
2026-09-17 17:24:54 -0400

    Moved 2 url constants into GeoUtilities and added comments in guide.md

--- packages/geoview-core/src/geo/utils/utilities.ts
+++ packages/geoview-core/src/geo/utils/utilities.ts
@@ -77,6 +77,13 @@ export abstract class GeoUtilities {
    */
   static #DEBUG_FORCE_PROXY = false;
 
+  /** The base URL for EPSG.io, used for projection lookups. */
+  static readonly EPSG_URL = 'https://epsg.io';
+
+  /** The ISO reference url for the dimension interval tags */
+  static readonly ISO_8601_REF_DIMENSION_INTERVAL =
+    'https://docs.digi.com//resources/documentation/digidocs/90001488-13/default.htm#reference/r_iso_8601_duration_format.htm';
+
   /**
    * Checks if the provided proxy URL is an Esri proxy.
    *
